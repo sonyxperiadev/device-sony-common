3f368651e914ff0522cf21aee5c0239912479101
Dinesh K Garg <dineshg@codeaurora.org>
2014-02-03 14:03:33 -0800

    Place library in vendor folder on device
    All vendors developed libraries must be in vendor folder on
    device. Using appropriate directive to accomplish the objective.
    
    Change-Id: I4ed413b799c0b66a86321f799713068776fa538a

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -2,6 +2,8 @@ ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+
 sourceFiles := \
                cryptfs_hw.c
 
@@ -18,7 +20,6 @@ LOCAL_MODULE:= libcryptfs_hw
 LOCAL_SHARED_LIBRARIES := $(commonSharedLibraries)
 
 LOCAL_MODULE_OWNER := qcom
-LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 endif
