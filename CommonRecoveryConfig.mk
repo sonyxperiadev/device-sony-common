2ec1cbf6eb9b180bb5f7559627c82f6b686549f3
Max Weffers <rcstar6696@gmail.com>
2016-02-14 11:03:45 +0100

    Add recovery Makefiles

---
+++ CommonRecoveryConfig.mk
@@ -0,0 +1,37 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TWRP UI
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_NO_SCREEN_BLANK := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# TWRP Storage
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TARGET_NO_SEPARATE_RECOVERY := true
+
+# TWRP Crypto
+TW_INCLUDE_L_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/bootdevice/by-name/userdata"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
