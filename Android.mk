9820cd56d969945377dcc09ac10bfa04af8bf7b4
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-04-06 10:06:11 +0200

    unify shinano platform
    all shinano device are now using same shinano config
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter yukon rhine shinano shinano2 kanuti kitakami,$(PRODUCT_PLATFORM)),)
+ifneq ($(filter yukon rhine shinano kanuti kitakami,$(PRODUCT_PLATFORM)),)
 
 LOCAL_PATH := $(call my-dir)
 
