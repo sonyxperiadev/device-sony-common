d3cdb5eaab55cfab1557975dcb9713f9904381cd
Shane Francis <bigbeeshane@gmail.com>
2016-05-25 21:34:17 +0100

    Add Kitakami fingerprint HAL
      * This was based around the OnePlus Two TrustZone fingerprint HAL (found here
       https://github.com/CyanogenMod/android_device_oneplus_oneplus2/blob/cm-13.0/fingerprint)
    
      * Extra information was obtained from the Nexus 6P (angler kernel driver) (found here
       https://android.googlesource.com/kernel/msm/+/3d0a564505ab8452e7e6f52b972db386cc2f5f69)
    
      * Protocol informations was verified as it passed through the QSEECOM API to linux kerne
    
      Authors Jens Andersen & Shane Francis

---
+++ fingerprint/QSEEComFunc.c
@@ -0,0 +1,245 @@
+/*
+ * Copyright (C) 2016 Shane Francis / Jens Andersen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "QSEEComFunc.h"
+
+#define LOG_TAG "QSEE_WRAPPER"
+#define LOG_NDEBUG 0
+
+//#define USE_QSEE_WRAPPER 1
+
+#include <cutils/log.h>
+
+int open_handle()
+{
+
+#ifdef USE_QSEE_WRAPPER
+    printf("Open Target Lib : libDSEEComAPI\n");
+    mLibHandle = dlopen("libDSEEComAPI.so", RTLD_NOW);
+#else
+    printf("Open Target Lib : libQSEEComAPI\n");
+    mLibHandle = dlopen("libQSEEComAPI.so", RTLD_NOW);
+#endif
+
+    if (!mLibHandle) {
+        printf("Failed to initialize QSEECom API library\n");
+        return -1;
+    } else {
+        printf("Initialized QSEECom API library\n");
+
+        bool hasError = false;
+
+        mStartApp = (start_app_fn) dlsym(mLibHandle, "QSEECom_start_app");
+
+        if (!mStartApp) {
+            printf("Failed to initialize QSEECom_start_app API function\n");
+            hasError = true;
+        }
+
+        mStopApp = (shutdown_app_fn) dlsym(mLibHandle, "QSEECom_shutdown_app");
+
+        if (!mStopApp) {
+            printf("Failed to initialize QSEECom_shutdown_app API function");
+            hasError = true;
+        }
+
+        load_external_elf_fn = (load_external_elf) dlsym(mLibHandle, "QSEECom_load_external_elf");
+
+        if (!load_external_elf_fn) {
+            printf("Failed to initialize QSEECom_load_external_elf API function\n");
+            hasError = true;
+        }
+
+        unload_external_elf_fn = (unload_external_elf) dlsym(mLibHandle, "QSEECom_unload_external_elf");
+
+        if (!unload_external_elf_fn) {
+            printf("Failed to initialize QSEECom_unload_external_elf API function\n");
+            hasError = true;
+        }
+
+        register_listener_fn = (register_listener) dlsym(mLibHandle, "QSEECom_register_listener");
+
+        if (!register_listener_fn) {
+            printf("Failed to initialize QSEECom_register_listener API function\n");
+            hasError = true;
+        }
+
+        unregister_listener_fn = (unregister_listener) dlsym(mLibHandle, "QSEECom_unregister_listener");
+
+        if (!unregister_listener_fn) {
+            printf("Failed to initialize QSEECom_unregister_listener API function\n");
+            hasError = true;
+        }
+
+        send_cmd_fn = (send_cmd) dlsym(mLibHandle, "QSEECom_send_cmd");
+
+        if (!send_cmd_fn) {
+            printf("Failed to initialize QSEECom_send_cmd API function\n");
+            hasError = true;
+        }
+
+        send_modified_cmd_fn = (send_modified_cmd) dlsym(mLibHandle, "QSEECom_send_modified_cmd");
+
+        if (!send_modified_cmd_fn) {
+            printf("Failed to initialize QSEECom_send_modified_cmd API function\n");
+            hasError = true;
+        }
+
+        receive_req_fn = (receive_req) dlsym(mLibHandle, "QSEECom_receive_req");
+
+        if (!receive_req_fn) {
+            printf("Failed to initialize QSEECom_receive_req API function\n");
+            hasError = true;
+        }
+
+        send_resp_fn = (send_resp) dlsym(mLibHandle, "QSEECom_send_resp");
+
+        if (!send_resp_fn) {
+            printf("Failed to initialize QSEECom_send_resp API function\n");
+            hasError = true;
+        }
+
+        set_bandwidth_fn = (set_bandwidth) dlsym(mLibHandle, "QSEECom_set_bandwidth");
+
+        if (!set_bandwidth_fn) {
+            printf("Failed to initialize QSEECom_set_bandwidth API function\n");
+            hasError = true;
+        }
+
+        app_load_query_fn = (app_load_query) dlsym(mLibHandle, "QSEECom_app_load_query");
+
+        if (!app_load_query_fn) {
+            printf("Failed to initialize QSEECom_app_load_query API function\n");
+            hasError = true;
+        }
+
+
+        if (hasError) {
+            printf("Close Target Lib : libQSEEComAPI\n");
+            dlclose(mLibHandle);
+            mLibHandle = NULL;
+            return -1;
+        }
+
+    }
+
+    return 1;
+}
+
+
+int32_t qcom_km_ION_memalloc(struct qcom_km_ion_info_t *handle,
+                             uint32_t size)
+{
+    int32_t ret = 0;
+    int32_t iret = 0;
+    int32_t fd = 0;
+    unsigned char *v_addr;
+    struct ion_allocation_data ion_alloc_data;
+    int32_t ion_fd;
+    int32_t rc;
+    struct ion_fd_data ifd_data;
+    struct ion_handle_data handle_data;
+    /* open ION device for memory management
+     * O_DSYNC -> uncached memory
+    */
+    if(handle == NULL) {
+        ALOGE("Error:: null handle received");
+        return -1;
+    }
+    ion_fd  = open("/dev/ion", O_RDONLY | O_DSYNC);
+    if (ion_fd < 0) {
+        ALOGE("Error::Cannot open ION device");
+        return -1;
+    }
+    handle->ion_sbuffer = NULL;
+    handle->ifd_data_fd = 0;
+    /* Size of allocation */
+    ion_alloc_data.len = (size + 4095) & (~4095);
+    /* 4K aligned */
+    ion_alloc_data.align = 4096;
+    /* memory is allocated from EBI heap */
+    ion_alloc_data.heap_id_mask = ION_HEAP(ION_QSECOM_HEAP_ID);
+    /* Set the memory to be uncached */
+    ion_alloc_data.flags = 0;
+    /* IOCTL call to ION for memory request */
+    rc = ioctl(ion_fd, ION_IOC_ALLOC, &ion_alloc_data);
+    if (rc) {
+        ret = -1;
+        goto alloc_fail;
+    }
+
+    ifd_data.handle = ion_alloc_data.handle;
+
+    /* Call MAP ioctl to retrieve the ifd_data.fd file descriptor */
+    rc = ioctl(ion_fd, ION_IOC_MAP, &ifd_data);
+    if (rc) {
+        ret = -1;
+        goto ioctl_fail;
+    }
+    /* Make the ion mmap call */
+    v_addr = (unsigned char *)mmap(NULL, ion_alloc_data.len,
+                                   PROT_READ | PROT_WRITE,
+                                   MAP_SHARED, ifd_data.fd, 0);
+    if (v_addr == MAP_FAILED) {
+        ALOGE("Error::ION MMAP failed");
+        ret = -1;
+        goto map_fail;
+    }
+    handle->ion_fd = ion_fd;
+    handle->ifd_data_fd = ifd_data.fd;
+    handle->ion_sbuffer = v_addr;
+    handle->ion_alloc_handle.handle = ion_alloc_data.handle;
+    handle->sbuf_len = size;
+    return ret;
+map_fail:
+    if (handle->ion_sbuffer != NULL) {
+        iret = munmap(handle->ion_sbuffer, ion_alloc_data.len);
+        if (iret)
+            ALOGE("Error::Failed to unmap memory for load image. ret = %d", ret);
+    }
+ioctl_fail:
+    handle_data.handle = ion_alloc_data.handle;
+    if (handle->ifd_data_fd)
+        close(handle->ifd_data_fd);
+    iret = ioctl(ion_fd, ION_IOC_FREE, &handle_data);
+    if (iret) {
+        ALOGE("Error::ION FREE ioctl returned error = %d",iret);
+    }
+alloc_fail:
+    if (ion_fd > 0)
+        close(ion_fd);
+    return ret;
+}
+
+
+int32_t qcom_km_ion_dealloc(struct qcom_km_ion_info_t *handle)
+{
+    struct ion_handle_data handle_data;
+    int32_t ret = 0;
+    /* Deallocate the memory for the listener */
+    ret = munmap(handle->ion_sbuffer, (handle->sbuf_len + 4095) & (~4095));
+    if (ret) {
+        ALOGE("Error::Unmapping ION Buffer failed with ret = %d", ret);
+    }
+    handle_data.handle = handle->ion_alloc_handle.handle;
+    close(handle->ifd_data_fd);
+    ret = ioctl(handle->ion_fd, ION_IOC_FREE, &handle_data);
+    if (ret) {
+        ALOGE("Error::ION Memory FREE ioctl failed with ret = %d", ret);
+    }
+    close(handle->ion_fd);
+    return ret;
+}
---
+++ fingerprint/tz_api.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2016 Shane Francis / Jens Andersen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __TZAPI_H_
+#define __TZAPI_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FP_TZAPP_PATH "/system/vendor/firmware/"
+#define FP_TZAPP_NAME "tzfingerprint"
+
+#define KM_TZAPP_PATH "/firmware/image/"
+#define KM_TZAPP_NAME "kmota"
+
+#define BUFFER_SIZE 64
+
+//enumerate tz app command ID's
+enum fingerprint_cmd_t {
+    FPC_INIT = 0x00,
+    FPC_ENROLL_START = 0x03,
+    FPC_ENROLL_STEP = 0x04,
+    FPC_ENROLL_END = 0x05,
+    FPC_AUTH_START = 0x07,
+    FPC_AUTH_STEP = 0x08,
+    FPC_AUTH_END = 0x09,
+    FPC_CHK_FP_LOST = 0x0A,
+    FPC_SET_WAKE = 0x0B,
+    FPC_GET_WAKE_TYPE = 0x0C,
+    FPC_INIT_NEW_DB = 0x0F,
+    FPC_SET_FP_STORE = 0x1A,
+    FPC_GET_PRINT_ID = 0x1F,
+    FPC_SET_DB_DATA = 0x10,
+    FPC_GET_DB_LENGTH = 0x11,
+    FPC_GET_DB_DATA = 0x12,
+    FPC_GET_ID_LIST = 0x13,
+    FPC_GET_ID_COUNT = 0x14,
+    FPC_GET_DEL_PRINT = 0x15,
+    FPC_CAPTURE_IMAGE = 0x17,
+    FPC_GET_INIT_STATE = 0x19,
+    FPC_GET_AUTH_HAT = 0x21,
+    FPC_GET_DB_ID = 0x20,
+    FPC_VERIFY_AUTH_CHALLENGE = 0x22,
+    FPC_GET_AUTH_CHALLENGE = 0x23,
+    FPC_SET_AUTH_CHALLANGE = 0x24,
+    FPC_SET_INIT_DATA = 0x25,
+    FPC_INIT_UNK_0 = 0x2A,
+    FPC_INIT_UNK_1 = 0x2B,
+    FPC_GET_REMAINING_TOUCHES = 0x2C,
+    FPC_INIT_UNK_2 = 0x26,
+};
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t ret_val; //Some cases this is used for return value of the command
+    uint32_t length; //Some length of data supplied by previous modified command
+    uint32_t extra; //Some length of data supplied by previous modified command
+} fpc_send_std_cmd_t;
+
+
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t ret_val; //Some cases this is used for return value of the command
+    uint32_t length; //Some length of data supplied by previous modified command
+    uint32_t id; //Some length of data supplied by previous modified command
+} fpc_send_auth_cmd_t;
+
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t ret_val; //Some cases this is used for return value of the command
+    uint32_t na1; // always 0x45 ?
+    uint32_t na2; //???
+    uint32_t na3; //???
+    uint32_t na4; //???
+    uint32_t print_index;
+} fpc_send_enroll_start_cmd_t;
+
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t p1; //Some cases this is used for return value of the command
+    uint32_t p2; // always 0x45 ?
+    uint32_t p3; //???
+    uint32_t p4; //???
+    uint32_t p5; //???
+    uint32_t print_count;
+} fpc_get_pint_index_cmd_t;
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t v_addr; //Virtual address of ion mmap buffer
+    uint32_t length; //Length of data on ion buffer
+    uint32_t extra; //???
+} fpc_send_mod_cmd_t;
+
+
+typedef struct {
+    uint32_t cmd_id;
+    uint32_t v_addr;
+    uint64_t ret_val; //64bit int
+} fpc_send_int64_cmd_t;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
