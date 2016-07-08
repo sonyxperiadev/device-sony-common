43c867123ce8915326a13b951e7f6421adb6a868
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-07-08 10:20:40 +0200

    set Camera HAL version 1 as default
    the camera works better with HAL1 and this should be the default
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -254,7 +254,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=0 \
-    persist.camera.HAL3.enabled=1 \
+    persist.camera.HAL3.enabled=0 \
     persist.camera.ois.disable=0
 
 # Sensors debug
