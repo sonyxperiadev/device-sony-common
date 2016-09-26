43d6e06d7eb4da385a6e68858dd21dd159688819
Adrian DC <radian.dc@gmail.com>
2016-09-26 19:47:32 +0200

    sony: common: Add support for the tone platform
    Change-Id: I4325d73a5057f9facc07507ad1fccea5a565789c

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(filter yukon rhine shinano kanuti kitakami loire,$(PRODUCT_PLATFORM)),)
+ifneq ($(filter yukon rhine shinano kanuti kitakami loire tone,$(PRODUCT_PLATFORM)),)
 
 LOCAL_PATH := $(call my-dir)
 
