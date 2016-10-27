da5f99d962a7ca0d47c653b8b5bdcdf6120895cf
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-10-27 07:35:43 +0200

    switch to camera HAL3
    on android 7 we have to use HAL3 as default
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -258,7 +258,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=0 \
+    persist.camera.HAL3.enabled=1 \
     persist.camera.gyro.disable=1 \
     persist.camera.feature.cac=0 \
     persist.camera.ois.disable=0
