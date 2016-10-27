0e5dbd26a06dbaaf03f60fd5af184f7056c0bfdc
David <Davidteri91@gmail.com>
2016-10-27 14:06:18 +0200

    sony-common: add MASTER_SIDE_CP_TARGET_LIST flag
    picked from CAF and added only msm8996 platform
    
    needed by: https://android.googlesource.com/platform/hardware/qcom/display/+/android-7.1.0_r4/msm8996/common.mk#41
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- Android.mk
+++ Android.mk
@@ -5,6 +5,9 @@ LOCAL_PATH := $(call my-dir)
 #List of targets that use video hw
 MSM_VIDC_TARGET_LIST := msm8226 msm8916 msm8952 msm8974 msm8994 msm8996
 
+#List of targets that use master side content protection
+MASTER_SIDE_CP_TARGET_LIST := msm8996
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
