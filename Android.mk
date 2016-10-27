d2b2a86616ca0e9d13f5d63e9077d26966acac3f
David <Davidteri91@gmail.com>
2016-10-27 14:04:43 +0200

    sony-common: add MSM_VIDC_TARGET_LIST flag
    picked from CAF and added only our supported platforms
    
    needed by: https://android.googlesource.com/platform/hardware/qcom/display/+/android-7.1.0_r4/msm8996/common.mk#37
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,6 +2,9 @@ ifneq ($(filter yukon rhine shinano kanuti kitakami loire tone,$(PRODUCT_PLATFOR
 
 LOCAL_PATH := $(call my-dir)
 
+#List of targets that use video hw
+MSM_VIDC_TARGET_LIST := msm8226 msm8916 msm8952 msm8974 msm8994 msm8996
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
