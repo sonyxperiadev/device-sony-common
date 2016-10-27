CyanogenMod/android_device_qcom_common

build: invoke utils.mk from device/qcom/common
utils.mk has macros that help with featurization based on
TARGET_PRODUCT, VENDOR etc.

Change-Id: Ie6610ce5ee986bd47cb8cb1a443a7fff04fea45c

common.mk
@@ -497,7 +497,7 @@ PRODUCT_COPY_FILES := \
 #    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
 
 # include additional build utilities
--include vendor/qcom/proprietary/common/build/utils.mk
+-include device/qcom/common/utils.mk
 
 #Enabling Ring Tones
 #include frameworks/base/data/sounds/OriginalAudio.mk

sonyxperiadev/device-sony-common

common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# include additional build utilities
+-include device/sony/common/utils.mk
+
 DEVICE_PACKAGE_OVERLAYS += device/sony/common/overlay
 
 # Common Specific Permissions