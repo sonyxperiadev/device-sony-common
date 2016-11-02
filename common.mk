fcb8cfa38f995926350674af64cb3534bace681d
Janis Danisevskis <jdanis@google.com>
2016-11-02 10:11:15 +0000

    Add keymaster HAL implementation and service
    Test: no
    Bug: 32020919
    Change-Id: I794b54679edeeb680c7e7d57eed4bd4d819d84f4

--- common.mk
+++ common.mk
@@ -81,7 +81,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.gatekeeper@1.0-impl
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.keymaster@3.0-impl
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
