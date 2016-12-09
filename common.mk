a1a9787fa494c902a8999d81f0191815a16c89ba
Roshan Pius <rpius@google.com>
2016-12-08 18:08:55 -0800

    Add wifi HAL service to device.mk
    Bug: 33460969
    Test: Compiles & the device boots up
    Change-Id: I32fcf553d09aa5dbcc3225625a07fe48a4ae3584

--- common.mk
+++ common.mk
@@ -83,7 +83,8 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.wifi@1.0-service
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
