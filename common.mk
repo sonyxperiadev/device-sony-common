5f95306d0334ec253be4dc81249894af177d8e9c
David <Davidteri91@gmail.com>
2016-12-12 09:34:15 +0100

    sony-common: enable sdcardFS support
    https://android.googlesource.com/platform/system/core/+/android-7.0.0_r24/sdcard/sdcard.c#96
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- common.mk
+++ common.mk
@@ -274,3 +274,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=0 \
     debug.qualcomm.sns.hal=0 \
     debug.qualcomm.sns.libsensor1=0
+
+# sdcardFS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
