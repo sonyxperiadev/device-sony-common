b7559b8d9623bc6272a83758ec1498d6bb4e1ed9
David <Davidteri91@gmail.com>
2017-01-26 12:48:38 +0100

    sony-common: healthd: update healthd header path for N
    healthd header was moved from system/core/healthd to system/core/healthd/include/healthd
    update the include paths to prevent errors in build due to missing #include healthd/healthd.h
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -6,7 +6,7 @@ LOCAL_SRC_FILES := healthd_board_sony.cpp
 LOCAL_MODULE := libhealthd.$(TARGET_DEVICE)
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := \
-    system/core/healthd \
+    system/core/healthd/include \
     bootable/recovery
 
 include $(BUILD_STATIC_LIBRARY)
