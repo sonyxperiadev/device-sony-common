16defedd9ebd996cffacce35075ba10f4fcaee7e
Jens Andersen <jens.andersen@gmail.com>
2017-04-28 18:01:19 +0200

    Refactor QSEEComApi implementation and usage
    Also various minor fixes:
    * Fix some exit codes being int32 and returning -1
    * Move trustload loading code common
    * Fix ignored sb_size in load_trustlet (but hardcode to minimum 1024 for now)

--- fingerprint/QSEEComFunc.h
+++ fingerprint/QSEEComFunc.h
@@ -20,38 +20,58 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #include <linux/ioctl.h>
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <fcntl.h> // open function
 #include <unistd.h> // close function
 #include <linux/msm_ion.h>
 #include "QSEEComAPI.h"
 
-typedef int (*start_app_fn)(struct QSEECom_handle **clnt_handle, const char *path, const char *fname, uint32_t sb_size);
-typedef int (*shutdown_app_fn)(struct QSEECom_handle **clnt_handle);
-typedef int (*load_external_elf)(struct QSEECom_handle **clnt_handle, const char *path,  const char *fname);
-typedef int (*unload_external_elf)(struct QSEECom_handle **handle);
-typedef int (*register_listener)(struct QSEECom_handle **handle, uint32_t lstnr_id, uint32_t sb_length, uint32_t flags);
-typedef int (*unregister_listener)(struct QSEECom_handle *handle);
-typedef int (*send_cmd)(struct QSEECom_handle *handle, void *send_buf, uint32_t sbuf_len, void *rcv_buf, uint32_t rbuf_len);
-typedef int (*send_modified_cmd)(struct QSEECom_handle *handle, void *send_buf, uint32_t sbuf_len, void *resp_buf, uint32_t rbuf_len, struct QSEECom_ion_fd_info  *ifd_data);
-typedef int (*receive_req)(struct QSEECom_handle *handle, void *buf, uint32_t len);
-typedef int (*send_resp)(struct QSEECom_handle *handle, void *send_buf, uint32_t len);
-typedef int (*set_bandwidth)(struct QSEECom_handle *handle, bool high);
-typedef int (*app_load_query)(struct QSEECom_handle *handle, char *app_name);
+// Forward declarations
+struct qcom_km_ion_info_t;
+struct qsee_handle_t;
 
-// DLSYM function pointers
-start_app_fn mStartApp;
-shutdown_app_fn mStopApp;
-load_external_elf load_external_elf_fn;
-unload_external_elf unload_external_elf_fn;
-register_listener register_listener_fn;
-unregister_listener unregister_listener_fn;
-send_cmd send_cmd_fn;
-send_modified_cmd send_modified_cmd_fn;
-receive_req receive_req_fn;
-send_resp send_resp_fn;
-set_bandwidth set_bandwidth_fn;
-app_load_query app_load_query_fn;
+
+// QCOM library functions
+typedef int (*start_app_def)(struct QSEECom_handle **clnt_handle, const char *path, const char *fname, uint32_t sb_size);
+typedef int (*shutdown_app_def)(struct QSEECom_handle **clnt_handle);
+typedef int (*load_external_elf_def)(struct QSEECom_handle **clnt_handle, const char *path,  const char *fname);
+typedef int (*unload_external_elf_def)(struct QSEECom_handle **handle);
+typedef int (*register_listener_def)(struct QSEECom_handle **handle, uint32_t lstnr_id, uint32_t sb_length, uint32_t flags);
+typedef int (*unregister_listener_def)(struct QSEECom_handle *handle);
+typedef int (*send_cmd_def)(struct QSEECom_handle *handle, void *send_buf, uint32_t sbuf_len, void *rcv_buf, uint32_t rbuf_len);
+typedef int (*send_modified_cmd_def)(struct QSEECom_handle *handle, void *send_buf, uint32_t sbuf_len, void *resp_buf, uint32_t rbuf_len, struct QSEECom_ion_fd_info  *ifd_data);
+typedef int (*receive_req_def)(struct QSEECom_handle *handle, void *buf, uint32_t len);
+typedef int (*send_resp_def)(struct QSEECom_handle *handle, void *send_buf, uint32_t len);
+typedef int (*set_bandwidth_def)(struct QSEECom_handle *handle, bool high);
+typedef int (*app_load_query_def)(struct QSEECom_handle *handle, char *app_name);
+
+// Utility functions
+typedef int32_t (*ion_free_def)(struct qcom_km_ion_info_t *handle);
+typedef int32_t (*ion_alloc_def)(struct qcom_km_ion_info_t *handle, uint32_t size);
+typedef int32_t (*load_trustlet_def)(struct qsee_handle_t* qsee_handle, struct QSEECom_handle **clnt_handle,const char *path, const char *fname, uint32_t sb_size);
+
+
+typedef struct qsee_handle_t {
+    void *_data;
+    // QCOM lib functions
+    start_app_def start_app;
+    shutdown_app_def shutdown_app;
+    load_external_elf_def load_external_elf;
+    unload_external_elf_def unload_external_elf;
+    register_listener_def register_listener;
+    unregister_listener_def unregister_listener;
+    send_cmd_def send_cmd;
+    send_modified_cmd_def send_modified_cmd;
+    receive_req_def receive_req;
+    send_resp_def send_resp;
+    set_bandwidth_def set_bandwidth;
+    app_load_query_def app_load_query;
+    // Utility functions provided by this library
+    ion_free_def ion_free;
+    ion_alloc_def ion_alloc;
+    load_trustlet_def load_trustlet;
+} qsee_handle_t;
 
 struct qcom_km_ion_info_t {
     int32_t ion_fd;
@@ -61,13 +81,8 @@ struct qcom_km_ion_info_t {
     uint32_t sbuf_len;
 };
 
-static void *mLibHandle = NULL;
-
-int open_handle();
-
-int32_t qcom_km_ion_dealloc(struct qcom_km_ion_info_t *handle);
-int32_t qcom_km_ION_memalloc(struct qcom_km_ion_info_t *handle, uint32_t size);
-
+int qsee_open_handle(struct qsee_handle_t **handle);
+int qsee_free_handle(struct qsee_handle_t** handle);
 char* qsee_error_strings(int err);
 
 #endif
