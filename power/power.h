a35d81682d29ab9a19321f6fffc14f0a8d23c8fb
Angelo G. Del Regno <kholk11@gmail.com>
2017-05-09 22:44:19 +0200

    PowerHAL: Implement PowerServer, timed optimizations, internal HALEXT
    Implement the PowerHAL PowerServer. This is responsible for
    communicating with external libraries (for now, the only user
    is librqbalance) to get custom optimization parameters.
    The PowerHAL PowerServer, an UNIX sockets server implementation,
    was made because of a quirk in Android, which makes IPC between
    32<->64 bit library practically impossible in standard ways.
    
    Timed optimizations implement POSIX timers in the HAL, responsible
    for setting up a timer after which the specified optimization
    (mincores, maxcores, balance level, frequency tweaks etc) will
    be discarded and reverted to sane platform defaults.
    This is necessary, for example, when we want to set specific
    optimizations that will make the SoC to draw much more power
    in very busy situations: here we want to minimize the IPC and
    wasted cpu cycles to calculate.
    A good example would be this:
    1. User starts video encoding through camcorder
    2. Camcorder needs to allocate buffers and initiate hw encoder
    3. Camcorder will start recording
    
    On step 2, we may want to push the SoC busses to the MAX to get
    maximum bandwidth on both RAM and SDHCI controller, which will
    make the camcorder application to allocate the required buffers
    and the file on SDCard/eMMC in a very small amount of time.
    While pushing up the frequencies will draw a lot of power,
    finishing the task in WAY LESS time will make us save time AND
    eventually also a small amount of power (computation at low
    power for 1 second would draw more power compared to high power
    for 50msec).
    Of course, the technique applies to many more situations.
    
    About the internal HAL Extension: this is still uncertain. I
    MAY put it back to be a totally external library for more
    freedom, but both of the solutions have their drawbacks.

--- power/power.h
+++ power/power.h
@@ -42,6 +42,10 @@
 #define MODE_STR_BALANCED		"normal"
 #define MODE_STR_PERFORMANCE		"perf"
 
+#define POWERSERVER_DIR			"/data/misc/powerhal/"
+#define POWERSERVER_SOCKET		POWERSERVER_DIR "rqbsvr"
+#define POWERSERVER_MAXCONN		10
+
 typedef int (*lock_acq_t)(int, int, int*, int);
 typedef int (*lock_rel_t)(int);
 
@@ -61,6 +65,39 @@ struct rqbalance_params {
 	char balance_level[PROP_VALUE_MAX];
 };
 
+/*
+ * enum extended_hint_t
+ * Provides extended power hints
+ *
+ * POWER_HINT_EXT_LOCK_ACQUIRE
+ *   Acquires a performance lock in rqbalance driver.
+ *   Calls the perf_lock_acquire function with the specified parameters.
+ *   It accepts a struct rqbalance_ctl_locks parameter.
+ *
+ *       NO
+ *   It accepts a string parameter with the following format:
+ *   "id:%d time:%d locktype:%d lockstate:%d"
+ *
+ * POWER_HINT_EXT_LOCK_SET_PARAM
+ *   Sets specific custom configuration parameters for the performance lock.
+ *   Calls the lock_set_arg function with the specified parameters.
+ *   It accepts a string parameter with the following format:
+ *   "arg:%d"
+ *
+ * POWER_HINT_EXT_LOCK_RELEASE
+ *   Releases a performance lock in rqbalance driver.
+ *   Calls the perf_lock_release function with the lock ID parameter.
+ *   It accepts an integer parameter containing the lock ID.
+ */
+typedef enum {
+	POWER_HINT_EXT_LOCK_ACQUIRE	= 0x00007FE0,
+	POWER_HINT_EXT_LOCK_SET_PARAM	= 0x00007FE1,
+	POWER_HINT_EXT_LOCK_RELEASE	= 0x00007FE2,
+	POWER_HINT_EXT_CLIENT_CONN	= 0x00007FE3,
+} extended_hint_t;
+
 /* Exported functions */
-void __set_power_mode(struct rqbalance_params *rqparm);
+void __set_special_power_mode(char* max_cpus, char* min_cpus,
+                              char* up_thresholds, char* down_thresholds,
+                              char* balance_level);
 void set_power_mode(rqb_pwr_mode_t mode);
