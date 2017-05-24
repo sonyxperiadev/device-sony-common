14bbe6de1c8b4dc452ae191beb3b90c4a42854e6
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-05-24 15:41:28 +0200

    set QCOM_MEDIA_ROOT
    since our project uses direct HAL selection we have to
    specify the QCOM_MEDIA_ROOT to be able to compile the code
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- Android.mk
+++ Android.mk
@@ -95,6 +95,7 @@ media-hal := hardware/qcom/media/msm8996
 endif
 
 ifeq ($(TARGET_KERNEL_VERSION),4.4)
+QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
 display-hal := hardware/qcom/display/msm8998
 media-hal := hardware/qcom/media/msm8998
 endif
