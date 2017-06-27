2c0cc32c9d725ed233b36dd132adda61b94dcb16
jiwon912.choi <jiwon912.choi@lge.com>
2017-06-27 16:21:51 +0900

    liblight: Change method to get LP mode brightness
    Bug : 62663757
    
    If sys.display.low_persistence_mode_brightness is set, get brightness
    from sys.display.low_persistence_mode_brightness.
    
    Change-Id: I21a7583f535f8797300bab738cc4c8805ca06b90

--- liblights/lights.c
+++ liblights/lights.c
@@ -18,6 +18,7 @@
 #define LOG_TAG "lights.sony"
 
 #include <cutils/log.h>
+#include <cutils/properties.h>
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -37,6 +38,8 @@
 /******************************************************************************/
 
 #define DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS 255
+
+#define LP_MODE_BRIGHTNESS_PROPERTY "sys.display.low_persistence_mode_brightness"
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -168,8 +171,10 @@ set_light_backlight(struct light_device_t* dev,
                         strerror(errno));
             }
             if (lpEnabled != 0) {
-                // This is defined in BoardConfig.mk.
-                brightness = DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS;
+                // Try to get the brigntess though property, otherwise it will
+                // set the default brightness, which is defined in BoardConfig.mk.
+                brightness = property_get_int32(LP_MODE_BRIGHTNESS_PROPERTY,
+                        DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS);
             }
         }
         g_last_backlight_mode = state->brightnessMode;
