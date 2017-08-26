fad4304ff6ac34b2c5f6d126021ef192a39121ee
David <Davidteri91@gmail.com>
2017-08-26 12:48:40 +0200

    Enable RenderScript Service
    igned-off-by: David Viteri <davidteri91@gmail.com>

--- common.mk
+++ common.mk
@@ -92,7 +92,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.composer@2.1-impl
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.renderscript@1.0-impl
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += \
