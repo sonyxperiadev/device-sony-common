0f08899e9c1b6b7f31993988f862c8ef540c7d25
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-08-28 14:28:17 +0200

    add fingerprint HAL for Android 8
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- treble.mk
+++ treble.mk
@@ -44,6 +44,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
