da521ed10426041cc5b2cb2e79112c209d6a9532
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-08-28 14:20:00 +0200

    add vibrator HAL for Android 8
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- treble.mk
+++ treble.mk
@@ -21,6 +21,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # Usb HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
