9ed3c149f2e466901923a55485e0c27319efd45d
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-08-28 14:22:40 +0200

    add NFC HAL for Android 8
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- treble.mk
+++ treble.mk
@@ -20,6 +20,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl
 
+# NFC packages
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
