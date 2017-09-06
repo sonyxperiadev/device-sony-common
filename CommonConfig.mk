88dac6df2dc97c09b335d81f37672cd67a8f76a8
David <Davidteri91@gmail.com>
2017-09-06 22:52:31 +0200

    sony-common: add Vendor Interface Object
    kanged from marlin
    https://android.googlesource.com/device/google/marlin/+/android-8.0.0_r3/compatibility_matrix.xml
    https://android.googlesource.com/device/google/marlin/+/android-8.0.0_r3/manifest.xml
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -92,3 +92,6 @@ BUILD_KERNEL := true
 
 # SELinux
 include device/sony/sepolicy/sepolicy.mk
+
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(COMMON_PATH)/compatibility_matrix.xml
