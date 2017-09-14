4c497d2721bf35f7f332471fa81d29a7f0b26211
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-14 13:45:00 +0200

    add libLLVM package
    the libLLVM library is a dependency of libRSDriver_adreno and
    we have to include it in the build
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librsjni
 
+# libRSDriver_adreno dependency
+PRODUCT_PACKAGES += \
+    libLLVM
+
 # ExtendedSettings
 PRODUCT_PACKAGES += \
     ExtendedSettings
