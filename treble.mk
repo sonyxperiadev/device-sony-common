e454a21fb4251c4b62d59806e7e74be494bcca29
David <Davidteri91@gmail.com>
2017-09-16 23:18:44 +0200

    sony-common: binderize Bluetooth
    to use HIDL we need add bluetooth services
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- treble.mk
+++ treble.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # NFC packages
 PRODUCT_PACKAGES += \
