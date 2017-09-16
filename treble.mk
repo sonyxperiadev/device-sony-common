a6bcc7a4e710d0d1e14f7a1e9b2073ec0cafefe7
David <Davidteri91@gmail.com>
2017-09-17 01:20:07 +0200

    sony-common: enable DRM service
    needed by aosp drm impl
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- treble.mk
+++ treble.mk
@@ -77,7 +77,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Usb HAL
 PRODUCT_PACKAGES += \
