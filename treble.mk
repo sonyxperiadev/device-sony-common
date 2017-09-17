6e032b5b5b7e57d61a0ba8e392a84f94ba859f5b
David <Davidteri91@gmail.com>
2017-09-17 12:00:43 +0200

    sony-common: add Keymaster service
    needed by Keystore and vold
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- treble.mk
+++ treble.mk
@@ -69,7 +69,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
