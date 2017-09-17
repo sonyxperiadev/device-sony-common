dec7ec89c85815198bc01d18b83282a7dd4ae866
David <Davidteri91@gmail.com>
2017-09-17 12:07:16 +0200

    sony-common: add Vibrator service
    needed by default impl
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- treble.mk
+++ treble.mk
@@ -63,7 +63,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \
