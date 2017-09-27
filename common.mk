c3b8af615837c81a57e8954d392780703e92c8cb
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-27 14:08:39 +0200

    move rild.libpath to common
    the rild.libpath is common to all devices and should not be
    declared in each platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -311,3 +311,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # sdcardFS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
+
+# RILD
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM
