b282083847f82c4ebc787f7c836117b71e93c4e1
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-28 15:34:40 +0200

    add missing android.hardware.nfc.hce.xml
    permission is needed by some apps
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -40,6 +40,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
