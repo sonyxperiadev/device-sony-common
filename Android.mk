c02b8a09f938bbc24036392247db89f502251509
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-30 20:00:15 +0200

    add NFC firmware compatibility link
    change is needed because upstream the NFC firmware has moved to
    /vendor/etc/firmware/
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- Android.mk
+++ Android.mk
@@ -4,4 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/firmware)
+$(shell pushd $(PRODUCT_OUT)/system/vendor/firmware > /dev/null && ln -s /vendor/etc/firmware/libpn547_fw.so libpn547_fw.so && popd > /dev/null)
+
 endif
