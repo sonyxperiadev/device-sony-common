6ac69b6d4270947a7ab47cd5a1a2ea7cb4d93fef
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-10-04 09:57:06 +0200

    init: move services from init.common.rc
    services are moved to init.common.srv.rc for easy
    integration into the new platforms
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
 # Common init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.common.rc:root/init.common.rc \
+    $(COMMON_PATH)/rootdir/init.common.srv.rc:root/init.common.srv.rc \
     $(COMMON_PATH)/rootdir/init.common.usb.rc:root/init.common.usb.rc \
     $(COMMON_PATH)/rootdir/init.common.usb-legacy.rc:root/init.common.usb-legacy.rc
 
