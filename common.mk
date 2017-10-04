f26cf8263b21f9417eca58d2bf556cfeafd9e479
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-10-04 12:15:07 +0200

    load libril-qc-qmi-1.so from /odm partition
    on new devices we load libril-qc-qmi-1.so from /odm partition
    on legacy devices we override rild.libpath from device's common git
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- common.mk
+++ common.mk
@@ -341,7 +341,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RILD
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    rild.libpath=/odm/lib64/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM
 
 PRODUCT_CUSTOM_IMAGE_MAKEFILES := $(COMMON_PATH)/odm.mk
