e0209a9a43cc25ccb4434d7f5fe9f9b3b94bd96e
David <Davidteri91@gmail.com>
2017-10-17 23:56:30 +0200

    common: remove not needed mapper service
    simple ... mapper has not init service
    check
    https://android.googlesource.com/platform/hardware/interfaces/+/android-8.0.0_r15/graphics/mapper/2.0/default/
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- treble.mk
+++ treble.mk
@@ -8,8 +8,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.graphics.mapper@2.0-service
+    android.hardware.graphics.mapper@2.0-impl
 
 # Memtrack
 PRODUCT_PACKAGES += \
