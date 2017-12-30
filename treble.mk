a8e1d51ba34a6c692e1b17efa4db84ff984f6eb8
Alin Jerpelea <alin.jerpelea@sony.com>
2017-12-30 08:14:18 +0100

    treble: add bluetooth@1.0-impl-qti
    on Android 8 wcnss_filter is deprecated and replaced by the HIDL
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- treble.mk
+++ treble.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl-qti \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
