f82e9af86b0ae553a31eb2d6a458c27eba99b47a
Alin Jerpelea <alin.jerpelea@sony.com>
2017-12-31 16:27:22 +0100

    move sensors configuration to vendor
    PLEASE NOTE
    This change needs Vendor v7 or newer
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -69,8 +69,11 @@ PRODUCT_COPY_FILES += \
 # Common etc
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/etc/gps.conf:system/etc/gps.conf \
-    $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(COMMON_PATH)/rootdir/system/etc/sensors/sensors_settings:system/etc/sensors/sensors_settings
+    $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml
+
+# Sensors common
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/vendor/etc/sensors/sensors_settings:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensors_settings
 
 # QMI
 PRODUCT_COPY_FILES += \
