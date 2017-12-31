bdcf83522cb96a78822c89288cd0dfe1a97c6766
oshmoun <oshmoun100@googlemail.com>
2017-12-31 15:44:35 +0100

    gps: switch to an updated hal
    The msm8994 gps hal we are using is obsolete. Switch to using a more up-to-date hal.
    
    Change-Id: I4449e66f772c298a040d71cf8d3aca14b290aa07

--- treble.mk
+++ treble.mk
@@ -53,7 +53,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti
 
 # Light
 PRODUCT_PACKAGES += \
