8e9dbef113027bab8ef4e55f71da18b716bd5b4a
Humberto Borba <humberos@gmail.com>
2018-01-01 12:50:35 -0200

    sony: common: Add gps_debug.conf
    It is requires for new GPS hal.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I2cb212732701a37e9eed8abf2fce0c39aed32a46

--- common.mk
+++ common.mk
@@ -72,7 +72,8 @@ PRODUCT_COPY_FILES += \
 
 # GPS Configuration
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/vendor/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
+    $(COMMON_PATH)/rootdir/vendor/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(COMMON_PATH)/rootdir/system/etc/gps_debug.conf:system/etc/gps_debug.conf
 
 # Sensors common
 PRODUCT_COPY_FILES += \
