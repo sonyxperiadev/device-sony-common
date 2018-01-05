5483388efa1025e2fa5732e16366a6df3a4b1ba6
Alin Jerpelea <alin.jerpelea@sony.com>
2018-01-05 09:59:02 +0100

    move hal selection to a separate mk
    we are ignoring the AOSP HAL selection and we are selecting
    the matching HALS from hardware/qcom/ in a separate mk
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ hardware/qcom/Android.mk
@@ -0,0 +1,21 @@
+#List of targets that use video hw
+MSM_VIDC_TARGET_LIST := msm8226 msm8916 msm8952 msm8974 msm8994 msm8996 msm8998
+
+#List of targets that use master side content protection
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998
+
+audio-hal := hardware/qcom/audio
+gps-hal := hardware/qcom/gps/sdm845
+display-hal := hardware/qcom/display/msm8998
+QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
+OMX_VIDEO_PATH := mm-video-v4l2
+media-hal := hardware/qcom/media/msm8998
+
+include $(display-hal)/Android.mk
+include $(call all-makefiles-under,$(audio-hal))
+include $(call all-makefiles-under,$(gps-hal))
+include $(call all-makefiles-under,$(media-hal))
+
+ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+  include $(call all-makefiles-under,hardware/qcom/bt/msm8998)
+endif
