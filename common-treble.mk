771a2f88eb8dc7ab930eacbd4532ee821b5323d3
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    sony-common: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen

--- common-treble.mk
+++ common-treble.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl:32 \
-    android.hardware.soundtrigger@2.0-impl:32
+    android.hardware.soundtrigger@2.1-impl:32
 
 # Camera
 PRODUCT_PACKAGES += \
