ddccd92997a2b0cbb96732bc331091ccd823965f
Alin Jerpelea <alin.jerpelea@sony.com>
2018-01-11 09:53:44 +0100

    qca_cld symlink cleanup
    the configuration files are in place and the old symlinks can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- hardware/qca_cld3/Android.mk
+++ hardware/qca_cld3/Android.mk
@@ -1,6 +1,4 @@
 ifeq ($(WIFI_DRIVER_BUILT),qca_cld3)
 $(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld)
-$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -s /odm/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -s /odm/firmware/wlan/qca_cld/bdwlan.bin firmware/wlan/qca_cld/bdwlan.bin && popd > /dev/null)
 $(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -s /data/misc/wifi/wlan_mac.bin firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
 endif
