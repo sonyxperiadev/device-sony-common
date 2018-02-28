f923b1984e7a64a0bfe2e90c8c497953247b73eb
Alin Jerpelea <alin.jerpelea@sony.com>
2018-02-28 13:58:08 +0100

    import qcom/utils.mk from wahoo
    qcom/utils.mk is copyed from wahoo git and included from hardware/qcom/Android.mk
    
    this change is needed to avoid issues with the qcom build environment
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -1,3 +1,7 @@
+# Board platforms lists to be used for
+# TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS += msm8952 msm8996 msm8998 sdm660
+
 #List of targets that use video hw
 MSM_VIDC_TARGET_LIST := msm8226 msm8916 msm8952 msm8974 msm8994 msm8996 msm8998 sdm660
 
@@ -10,6 +14,8 @@ display-hal := hardware/qcom/display/msm8998
 QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
 OMX_VIDEO_PATH := mm-video-v4l2
 media-hal := hardware/qcom/media/msm8998
+
+include device/sony/common/hardware/qcom/utils.mk
 
 include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
