328623fec13056ff64db8a1e6b51c86d6ca05e4d
Alin Jerpelea <alin.jerpelea@sony.com>
2018-03-05 16:15:05 +0100

    simplify init
    the init.common.rc is the minimum init to boot the device and should
    be moved to vendor/etc/init/hw
    
    please note that device specific init is moved to vendor/etc/init/init.<platform>.rc
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -22,6 +22,16 @@ LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := init.$(TARGET_DEVICE)
+LOCAL_SRC_FILES := vendor/etc/init/hw/init.common.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := init.$(TARGET_DEVICE)
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.usb.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := vendor/etc/init/init.usb.rc
