817ddb6463cdf0326a46a1bf0cb21ee6ef454cfd
oshmoun <oshmoun100@googlemail.com>
2018-03-22 00:46:42 +0100

    Build Sony fingerprint HIDL HAL
    Change-Id: Ib7a23e59f38a6fbeaba3060aaebca63447261f9c

--- common-treble.mk
+++ common-treble.mk
@@ -70,7 +70,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.sony
 
 # Keymaster
 PRODUCT_PACKAGES += \
