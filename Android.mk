e0011c1c45cdd07b149a93ae15145fded18860c7
Alin Jerpelea <alin.jerpelea@sony.com>
2018-03-21 09:41:50 +0100

    Revert "common: use the AOSP json-c"
    This reverts commit 5a20088b70290f4f17dd33902a0bf9d562119b59.

--- Android.mk
+++ Android.mk
@@ -4,8 +4,4 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-ifneq ($(TARGET_BOARD_PLATFORM),msm8998)
-  include hardware/qcom/msm8998/Android.mk
-endif
-
 endif
