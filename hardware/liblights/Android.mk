78b10ccd44c623af5e2798e10edae636352afd22
David Viteri <davidteri91@gmail.com>
2018-03-27 09:25:59 +0200

    lights: re-add kernel headers dependencies
    This is needed by inline kernel system 
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- hardware/liblights/Android.mk
+++ hardware/liblights/Android.mk
@@ -3,6 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
+ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 LOCAL_MODULE := android.hardware.light@2.0-service.sony
 LOCAL_INIT_RC := android.hardware.light@2.0-service.sony.rc
 LOCAL_SRC_FILES := \
