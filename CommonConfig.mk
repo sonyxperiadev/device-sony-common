65c13e44ea7e01b3a206d224995d068f94808e63
Alin Jerpelea <alin.jerpelea@sony.com>
2018-04-16 22:02:15 +0200

    disable Serial console
    The serial console is used only for debuggig and should be
    enabled only if it is needed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -29,7 +29,7 @@ TARGET_NO_KERNEL := false
 ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 endif
-BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
+#BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += coherent_pool=8M
 BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 user_debug=31
