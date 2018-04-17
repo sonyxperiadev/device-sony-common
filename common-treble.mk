0dbac4b3680f7b03c5e2743da605a6d1d2dfcc3c
David <Davidteri91@gmail.com>
2018-04-17 11:18:57 +0200

    common: binderize sensors HAL
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- common-treble.mk
+++ common-treble.mk
@@ -61,7 +61,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Vibrator
 PRODUCT_PACKAGES += \
