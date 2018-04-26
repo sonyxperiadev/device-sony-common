340765503b3e2fcdb6518e5a1326510dcb5d2d03
Angelo G. Del Regno <kholk11@gmail.com>
2018-04-26 19:16:13 +0200

    hardware: liblights: Add support for MicroCommunicator backlight
    Devices with projection unit require the new Micro Communicator
    to manage the same.
    Add support to send the backlight events to the MicroComm to
    be able to manage the projection light on these devices.
    
    This support gets enabled only when TARGET_USES_UCOMM_BACKLIGHT.

--- hardware/liblights/Android.mk
+++ hardware/liblights/Android.mk
@@ -7,6 +7,11 @@ ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
+
+ifeq ($(TARGET_USES_UCOMM_BACKLIGHT),true)
+    LOCAL_CFLAGS += -DUCOMMSVR_BACKLIGHT
+endif
+
 LOCAL_MODULE := android.hardware.light@2.0-service.sony
 LOCAL_INIT_RC := android.hardware.light@2.0-service.sony.rc
 LOCAL_SRC_FILES := \
@@ -22,5 +27,9 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     libhidltransport \
     android.hardware.light@2.0
+
+ifeq ($(TARGET_USES_UCOMM_BACKLIGHT),true)
+    LOCAL_SHARED_LIBRARIES += libucommunicator
+endif
 
 include $(BUILD_EXECUTABLE)
