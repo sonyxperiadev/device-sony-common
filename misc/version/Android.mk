c96a9634df8f9ce65a537f63307e237fe42b6854
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-11 16:28:09 +0200

    use default odm/build.prop for version check
    with a small patch we can use the default AOSP vendor check
    for this we add 2 properties:
    ro.odm.version and ro.odm.expected.version
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- misc/version/Android.mk
+++ misc/version/Android.mk
@@ -1,4 +1,1 @@
-$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.kernel.version=$(TARGET_KERNEL_VERSION)" >odm_version.prop && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.build.version=$(PLATFORM_VERSION)" >>odm_version.prop && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.platform.version=$(TARGET_BOARD_PLATFORM)" >>odm_version.prop && popd > /dev/null)
-$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.vendor.version=$(TARGET_VENDOR_VERSION)" >>odm_version.prop && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.odm.version=$(PLATFORM_VERSION)_$(TARGET_KERNEL_VERSION)_$(TARGET_BOOTLOADER_BOARD_NAME)_$(TARGET_VENDOR_VERSION)" >build.prop && popd > /dev/null)
