f7ef5cbec3710e1cc989a9f0ee7fc3bc0c853ed0
Christian Oder <myself5@carbonrom.org>
2018-05-12 23:37:54 +0200

    common: Unify ro.odm.* definition
    If we use different definitions chances are that we run into missmatches even if we don't want to.

--- misc/version/Android.mk
+++ misc/version/Android.mk
@@ -1,1 +1,1 @@
-$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.odm.version=$(PLATFORM_VERSION)_$(TARGET_KERNEL_VERSION)_$(TARGET_BOOTLOADER_BOARD_NAME)_$(TARGET_VENDOR_VERSION)" >build.prop && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/odm/ > /dev/null && echo "ro.odm.version=$(PLATFORM_VERSION)_$(SOMC_KERNEL_VERSION)_$(SOMC_PLATFORM)_$(TARGET_VENDOR_VERSION)" >build.prop && popd > /dev/null)
