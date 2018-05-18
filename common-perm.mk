2e1529d82262a364451b786b96ba09c767687f3c
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-18 10:05:00 +0200

    add privapp-permissions-extension.xml
    the privapp-permissions-extension.xml contains permisisons for
    packages used by the project
    
    Not whitelisted permissions are granted for package
    com.android.dialer: [android.permission.STATUS_BAR]
    
    Not whitelisted permissions are granted for package
    com.android.systemui: [com.android.permission.WRITE_EMBEDDED_SUBSCRIPTIONS]
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-perm.mk
+++ common-perm.mk
@@ -43,3 +43,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+
+# Common System Permissions
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/system/etc/permissions/privapp-permissions-extension.xml:system/etc/permissions/privapp-permissions-extension.xml
