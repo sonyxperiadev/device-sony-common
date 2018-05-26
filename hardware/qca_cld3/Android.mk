bcc428634e0c597f0f356f394c7f4651fdddceb2
Alin Jerpelea <alin.jerpelea@sony.com>
2018-05-26 10:13:54 +0200

    qcacld: force symlink creation
    when a rebuild is triggered there is a warning that the symlink
    can not be created. To supress the warning We can force the
    symlink creation
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- hardware/qca_cld3/Android.mk
+++ hardware/qca_cld3/Android.mk
@@ -1,4 +1,4 @@
 ifeq ($(WIFI_DRIVER_BUILT),qca_cld3)
 $(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld)
-$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -s /data/misc/wifi/wlan_mac.bin firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
+$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -sf /data/misc/wifi/wlan_mac.bin firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
 endif
