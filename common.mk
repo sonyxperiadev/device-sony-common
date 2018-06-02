0dbd4d7a3adead2e716c3c86a8214f27b04cebcd
Alin Jerpelea <alin.jerpelea@sony.com>
2018-06-02 16:39:41 +0200

    import customization from an external git
    we import common customization from an local git if this git exists
    
    This way custom projects can set locally the needed configuration
    without modifying the base one.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -100,3 +100,5 @@ $(call inherit-product, device/sony/common/common-packages.mk)
 $(call inherit-product, device/sony/common/common-perm.mk)
 $(call inherit-product, device/sony/common/common-prop.mk)
 $(call inherit-product, device/sony/common/common-treble.mk)
+
+$(call inherit-product-if-exists, device/sony/customization/customization.mk)
