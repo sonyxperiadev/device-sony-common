ec1586ef9e10190e38ca5b2589514f196255ab8d
Alin Jerpelea <alin.jerpelea@sony.com>
2018-07-16 11:29:24 +0200

    vendor: increment vendor version
    update vendor version to v14
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Vendor version
-TARGET_VENDOR_VERSION := v13
+TARGET_VENDOR_VERSION := v14
 
 # Release key
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/oss/release-keys/releasekey
