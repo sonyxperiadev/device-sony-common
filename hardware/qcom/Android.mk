44f223b21c36655b0bc2aed52f661ea0994f3b3a
Pavel Dubrova <pashadubrova@gmail.com>
2018-08-17 14:12:47 +0300

    hardware: qcom: Remove non k4.9 HALs inclusion
    This branch for Android P+ and it uses kernel 4.9 only.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -8,16 +8,6 @@ MSM_VIDC_TARGET_LIST := msm8952 msm8996 msm8998 sdm660 sdm845
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
-ifeq ($(SOMC_KERNEL_VERSION),4.4)
-audio-hal := hardware/qcom/audio
-bt-hal := hardware/qcom/bt/msm8998
-gps-hal := hardware/qcom/gps/sdm845
-display-hal := hardware/qcom/display/msm8998
-QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
-OMX_VIDEO_PATH := mm-video-v4l2
-media-hal := hardware/qcom/media/msm8998
-endif
-
 ifeq ($(SOMC_KERNEL_VERSION),4.9)
 audio-hal := hardware/qcom/audio
 bt-hal := hardware/qcom/bt/sdm845
