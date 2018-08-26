2517e3fc84ea225f9b141366bf600e54fa29fc27
David <Davidteri91@gmail.com>
2018-08-27 00:41:10 +0200

    treble: update drm implementation
    following wahoo android-9.0.0_r1
    
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- common-treble.mk
+++ common-treble.mk
@@ -105,8 +105,9 @@ endif
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-impl:32 \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Usb HAL
 PRODUCT_PACKAGES += \
