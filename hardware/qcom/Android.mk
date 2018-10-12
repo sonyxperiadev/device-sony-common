ec208507c7bcde028f937ab0444ac676334f889e
Alin Jerpelea <alin.jerpelea@sony.com>
2018-10-12 09:17:32 +0200

    Revert "k4.9: use sdm845 mediaHAL only for tama"
    This reverts commit 1185a240cc481db1182e8f51682956495fc9b5e5.

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -21,11 +21,7 @@ ifeq ($(SOMC_KERNEL_VERSION),4.9)
 audio-hal := hardware/qcom/audio
 gps-hal := hardware/qcom/gps/sdm845
 display-hal := hardware/qcom/display/sde
-ifneq ($(filter loire tone nile yoshino,$(SOMC_PLATFORM)),)
 QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
-else
-QCOM_MEDIA_ROOT := hardware/qcom/media/sdm845
-endif
 OMX_VIDEO_PATH := mm-video-v4l2
 media-hal := $(QCOM_MEDIA_ROOT)
 endif
