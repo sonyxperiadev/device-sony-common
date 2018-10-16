0d2fa68a2a0f005a3e8279e0ecb3cfada65b63bc
Alin Jerpelea <alin.jerpelea@sony.com>
2018-10-16 14:27:17 +0200

    update vendor version to v2
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -15,7 +15,7 @@
 $(call inherit-product-if-exists, device/sony/customization/customization.mk)
 
 # Vendor version
-TARGET_VENDOR_VERSION := v1
+TARGET_VENDOR_VERSION := v2
 
 # Specify a "dev-keys" configuration.  Keys from
 # vendor/oss/release-keys will be used instead of the keys under
