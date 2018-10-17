396d7d6851483f33764c5e563332c8d6daab6ba9
Alin Jerpelea <alin.jerpelea@sony.com>
2018-10-17 15:40:40 +0200

    gpt-utils: include log/log.h
    this header switch will silence the build time warning
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- hardware/gpt-utils/gpt-utils.cpp
+++ hardware/gpt-utils/gpt-utils.cpp
@@ -51,7 +51,7 @@
 #include <vector>
 #include <string>
 #define LOG_TAG "gpt-utils"
-#include <cutils/log.h>
+#include <log/log.h>
 #include <cutils/properties.h>
 #include "gpt-utils.h"
 #include <endian.h>
