a092e2cd48cbf0e68ce021644f5432894208dfb8
Alin Jerpelea <alin.jerpelea@sony.com>
2018-10-19 12:26:12 +0200

    add missing Treble radio packages
    Those pachages are dependencyes for the new Treble RIL
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-treble.mk
+++ common-treble.mk
@@ -21,7 +21,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2-radio-service
+    android.hardware.radio@1.2-radio-service \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0
 
 # Audio
 PRODUCT_PACKAGES += \
