dec9d98d3a39cfeab35c48d0f00dcd99d86dc544
oshmoun <oshmoun100@googlemail.com>
2018-10-26 03:14:30 +0200

    treble: switch to gnss 1.1
    Change-Id: I0e0ff708dd3674d2d52c04e166ab5a7d40316fc7

--- common-treble.mk
+++ common-treble.mk
@@ -56,8 +56,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti
 
 # Light
 PRODUCT_PACKAGES += \
