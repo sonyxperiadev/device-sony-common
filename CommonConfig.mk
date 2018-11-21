fcc9bb6920887fb1b7da1ec4b31e7d044e811287
Alin Jerpelea <alin.jerpelea@sony.com>
2018-11-21 04:49:53 +0100

    cmdline: Disable cgroup kernel memory extension
    copy of:
    commit c128d76f9bb37be511dea5775180b41965d29515
    Author: Suren Baghdasaryan <surenb@google.com>
    Date:   Wed Apr 18 10:27:00 2018 -0700
    
    crosshatch: Disable cgroup kernel memory extension
    
    Android is not using cgroup kernel memory extension features to account
    for kernel memory usage or to limit it in any way. As of Linux Kernel 4.6
    kernel memory extension is enabled by default. Explicitly disable kernel
    memory extension to avoid overhead of unused feature.
    
    Bug: 78151354
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -35,6 +35,7 @@ endif
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += coherent_pool=8M
 BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 user_debug=31
+BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
 BOARD_KERNEL_CMDLINE += printk.devkmsg=on
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
