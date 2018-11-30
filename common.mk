70e12118f26d7cec1c3ba434f3776326e1f88baa
Alin Jerpelea <alin.jerpelea@sony.com>
2018-11-30 13:24:06 +0100

    Revert "common: copy the default configuration for c2 codecs"
    This reverts commit a95035f34d18e470e23f0dcfbb8fa3638b4b7a8f.

--- common.mk
+++ common.mk
@@ -37,10 +37,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # Codecs Configuration
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
 # Common etc
 PRODUCT_COPY_FILES += \
