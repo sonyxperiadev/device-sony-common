0474cce8a805638383a6be1d25f1f4953db116d9
Alin Jerpelea <alin.jerpelea@sony.com>
2018-12-07 14:00:56 +0100

    firmware: create mount points for firmware at build time
    the A/B partition system requires that firmware mount points
    are created at build time
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

---
+++ hardware/firmware/Android.mk
@@ -0,0 +1,4 @@
+# Mount points
+$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/dsp)
+$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/firmware_mnt)
+$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/bt_firmware)
