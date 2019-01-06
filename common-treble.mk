21560545dda77796891fa2b331fc1872ebe2e40f
ix5 <ix5@users.noreply.github.com>
2019-01-06 21:33:30 +0100

    Remove deprecated backup healthd instance
    Our new health HAL enabes use to move to health@2.0 and deprecate
    healthd entirely.
    
    For now, AOSP ships a backup instance of healthd by default, so disable
    it in accordance with the official documentation:
    https://source.android.com/devices/tech/health/deprecation

--- common-treble.mk
+++ common-treble.mk
@@ -60,6 +60,10 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.sony
+# Remove deprecated backup healthd; this can be omitted
+# once healthd is no longer included by default.
+DEVICE_FRAMEWORK_MANIFEST_FILE += \
+    system/libhidl/vintfdata/manifest_healthd_exclude.xml
 
 # Sensors
 PRODUCT_PACKAGES += \
