340735da39ecc0906aec5311a5d1f318a755c02e
LuK1337 <priv.luk@gmail.com>
2019-02-23 03:25:30 +0100

    Drop android.hardware.power@1.0 service and impl
    * Since we are providing our own service and
      power.$(TARGET_DEVICE) is dead there's no
      reason to build it.

--- common-treble.mk
+++ common-treble.mk
@@ -128,11 +128,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
-
 ifeq ($(AB_OTA_UPDATER),true)
 # Boot control
 PRODUCT_PACKAGES += \
