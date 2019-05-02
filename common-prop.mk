6412fee3b9475c819b1b0b5fa79dfa9382187608
Alin Jerpelea <alin.jerpelea@sony.com>
2019-05-02 14:29:02 +0200

    common-prop: initial configuration for Android LMKD
    Starting in Android 9, userspace lmkd activates if an
    in-kernel lowmemorykiller driver is not detected.
    
    We add the initial configuration for Android LMKD
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-prop.mk
+++ common-prop.mk
@@ -108,6 +108,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7 \
     ro.config.media_vol_steps=25
 
+# Set lmkd options
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Property to enable user to access Google WFD settings.
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=0
