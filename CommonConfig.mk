53175ab8cb174db75939e1ee66366597d77133c2
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-07-24 18:24:26 +0200

    CommonConfig: Add 32-bits toolchain prefix for VDSO32
    The kernel recently got VDSO32 support and it now requires us
    to compile kernel code targeting ARM/AArch32 (the VDSO32 compat
    code does actually target 32-bits...) to get the feature to work.
    
    Define the 32-bits toolchain to use to compile the VDSO32 in kernel.

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -43,6 +43,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_32BITS_PREFIX := arm-linux-androideabi-
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Use mke2fs to create ext4 images
