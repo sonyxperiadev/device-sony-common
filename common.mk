c2c583f629c36609db507f1cce50e49e3573e909
ix5 <ix5@users.noreply.github.com>
2019-08-30 21:26:47 +0200

    common: Put more RROs into /vendor
    Since between the various devices we are modifying more overlays than
    just framework-res, we want our modifications to be treble-compatible
    and land in the /vendor partition as $APPNAME__auto_generated_rro.apk.
    
    Since https://r.android.com/810894 it is possible to add other targets
    than framework-res to the list of the packages for which RROs are
    generated.
    
    This commit aids treble compliance so that our devices retain their
    overlays when running GSIs.

--- common.mk
+++ common.mk
@@ -24,6 +24,11 @@ SONY_BUILD_SYMLINKS := $(COMMON_PATH)/sony_build_symlinks.mk
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
+    Bluetooth \
+    Settings \
+    SettingsProvider \
+    SystemUI \
+    Telephony \
     framework-res
 
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
