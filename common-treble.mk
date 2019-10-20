9c7e34f68764dbad502f0ee9c0077ec2e733d3c6
ix5 <ix5@users.noreply.github.com>
2019-10-20 11:49:50 +0200

    common-treble/vintf: Remove widevine refs
    The widevine service is proprietary and not shipped with AOSP. Declaring
    it in the vintf manifest will result in an endless loop of
    servicemanager trying to start the service.
    
    Downstream projects are advised to revert this commit if they are
    shipping the widevine blobs.

--- common-treble.mk
+++ common-treble.mk
@@ -100,8 +100,7 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey \
-    android.hardware.drm@1.1-service.widevine
+    android.hardware.drm@1.2-service.clearkey
 
 ifneq ($(BOARD_USE_LEGACY_USB),true)
 # Usb HAL
