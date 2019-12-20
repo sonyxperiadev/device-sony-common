69e4b35232b59b8bce29bf955a96398e27ba1ac2
ix5 <ix5@users.noreply.github.com>
2019-12-20 16:29:35 +0100

    [k4.9] common: Add 4.9 gatekeeper/keymaster compat
    This partially reverts commit 26af32cb806028d86cdb1376a0927e2e2db08cff.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -11,7 +11,28 @@ LOCAL_VENDOR_MODULE      := true
 include $(BUILD_PREBUILT)
 endif
 
-ifeq ($(TARGET_KEYMASTER_V4), true)
+ifneq ($(TARGET_LEGACY_KEYMASTER), true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.gatekeeper@1.0-service-qti
+LOCAL_SRC_FILES := vendor/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := android.hardware.gatekeeper@1.0-service-qti
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+ifneq ($(TARGET_KEYMASTER_V4), true)
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.keymaster@3.0-service-qti
+LOCAL_SRC_FILES := vendor/etc/init/android.hardware.keymaster@3.0-service-qti.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_STEM := android.hardware.keymaster@3.0-service-qti
+LOCAL_MODULE_SUFFIX := .rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+else
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.keymaster@4.0-service-qti
 LOCAL_SRC_FILES := vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc
@@ -21,7 +42,9 @@ LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
-endif # $(TARGET_KEYMASTER_V4) == true
+endif
+
+endif # $(TARGET_LEGACY_KEYMASTER) != true
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.qcom.slpistart.sh
