35745c5734c12eec963483e264158b7f386c7a60
Alin Jerpelea <alin.jerpelea@sony.com>
2020-01-30 13:04:10 +0100

    common-treble: add packages for Radio 1.1
    From version 4 we need Radio 1.1 packages
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-treble.mk
+++ common-treble.mk
@@ -38,7 +38,8 @@ PRODUCT_PACKAGES += \
 # RIL
 # Interface library needed by odm blobs:
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.0
+    android.hardware.radio@1.1 \
+    android.hardware.radio.config@1.1
 
 # Audio
 PRODUCT_PACKAGES += \
