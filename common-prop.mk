bbb2b24fb64516f3973154b86653096335d23c6f
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-05-17 17:30:40 +0200

    prop: Explicitly enable persist.vendor.radio.oem_socket
    Explicitly enable this in case RIL updates start interpreting
    this property as default-false.
    Please remember that this is strictly needed for features like
    IMS/VoLTE (and others).

--- common-prop.mk
+++ common-prop.mk
@@ -62,7 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES +=
     persist.vendor.radio.mt_sms_ack=19 \
     persist.vendor.radio.enableadvancedscan=true \
     persist.vendor.radio.unicode_op_names=true \
-    persist.vendor.radio.sib16_support=1
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.oem_socket=true
 
 # Ringer
 PRODUCT_PROPERTY_OVERRIDES += \
