sonyxperiadev/device-sony-common

commit fa14c9db6b4a6fd837eb771728e51cb7bc8eaa14

add dpm service init
The service appears to be used by modem

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

rootdir/Android.mk
@@ -244,6 +244,14 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := dpm.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := vendor/etc/init/dpmd.rc
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := qseecom.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := vendor/etc/init/qseecom.rc

common-init.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     ipacm.rc \
     dataqti.rc \
     dpmQmiMgr.rc \
+    dpmd.rc \
     imsdatadaemon.rc \
     imsqmidaemon.rc \
     imsrcsd.rc \

rootdir/vendor/etc/init/dpmd.rc
@@ -1,0 +1,12 @@
+# Create the directories used by DPM subsystem
+on post-fs-data
+    mkdir /data/dpm 0771 system system
+    chown system system /data/dpm
+
+#start dpmd service
+service dpmd /odm/bin/dpmd
+    class late_start
+    socket dpmd stream 660 root radio
+    socket tcm  stream 660 root inet
+    socket dpmwrapper stream 660 root inet
+    group system readproc inet radio wakelock

commit 4e08651e8c3bb40b71225871ac06ae47442b2045

rootdir: Fix dpmd module name
The file is named dpmd.rc, as is the dependency on it in common-init.mk.

Fixes: fa14c9db6b4a6fd837eb771728e51cb7bc8eaa14
Signed-off-by: Marijn Suijten <marijns95@gmail.com>

rootdir/Android.mk
@@ -244,7 +244,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := dpm.rc
+LOCAL_MODULE := dpmd.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := vendor/etc/init/dpmd.rc
 LOCAL_MODULE_TAGS := optional