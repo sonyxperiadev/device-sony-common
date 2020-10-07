d1e278e3e0a44b59991940e1fb96d179c4abad6f
Alin Jerpelea <alin.jerpelea@sony.com>
2020-10-07 10:10:57 +0200

    common: add imsrtp seccomp_policy
    This policy is mandatory for IMS services
    
    NOTE:
    This is a bare policy for now and will be updated later
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -97,6 +97,7 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/vendor/etc/seccomp_policy/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
     $(COMMON_PATH)/rootdir/vendor/etc/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Audio Configuration
