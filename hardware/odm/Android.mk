30915a9ebf19549be97ee65a3ea417bfd2174a98
ix5 <ix5@users.noreply.github.com>
2020-12-23 18:19:46 +0100

    odm: Add build.prop cleaner
    Certain props need to be filtered out from the proprietary
    odm image ("Software Binaries") because they are
    device-specific, while the odm image is released per
    platform.

---
+++ hardware/odm/Android.mk
@@ -0,0 +1,11 @@
+# Remove device-specific props from odm
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := odm_prop_cleaner
+LOCAL_MODULE_PATH := $(TARGET_OUT_ODM)
+
+LOCAL_POST_INSTALL_CMD := sed -i '/ro.product.odm/d' $(TARGET_OUT_ODM)/etc/build.prop
+
+include $(BUILD_PHONY_PACKAGE)
