8f1e132db5daa0cb65b609f050ec407e099e31ff
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2021-02-14 22:31:03 +0100

    vintf: Add QTI camera postprocessing interface manifest
    CamX starting from the kernel 4.19 based BSP needs us to load the
    postprocessing interface and this needs to have its own manifest
    entry.

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -218,6 +218,11 @@ else
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hardware.vibrator_v1.0.xml
 endif
 
+# CamX manifests
+ifneq ($(filter edo,$(SOMC_PLATFORM)),)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/vendor.qti.hardware.camera.postproc.xml
+endif
+
 # New vendor security patch level: https://r.android.com/660840/
 # Used by newer keymaster binaries
 VENDOR_SECURITY_PATCH=$(PLATFORM_SECURITY_PATCH)
