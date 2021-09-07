698a4e1d870793bbcc5129e89873dbd66d412bc9
Alin Jerpelea <alin.jerpelea@sony.com>
2021-09-07 11:13:47 +0200

    common-init: migrate sscrpcd service to bp
    migrate sscrpcd services to bp and remove ifdef
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,14 +19,3 @@ LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init/hw
 include $(BUILD_PREBUILT)
-
-
-ifneq ($(filter sdm660 msm8998 sdm845 sm8150,$(TARGET_BOARD_PLATFORM)),)
-include $(CLEAR_VARS)
-LOCAL_MODULE := sscrpcd.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := vendor/etc/init/sscrpcd.rc
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/init
-include $(BUILD_PREBUILT)
-endif # TARGET_BOARD_PLATFORM in (sdm660 msm8998 sdm845 sm8150)
