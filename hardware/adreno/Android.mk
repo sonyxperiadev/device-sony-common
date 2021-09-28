sonyxperiadev/device-sony-common

commit 0aecc4398e8f77eac2d354d5c9879eb18a92d584

hardware/adreno: Convert vendor->odm symlink to ro.hardware property
Android is continuously clamping down on symlink hacks (see the report
in [1]) and for a good reason: they provide a perfectly valid
alternative using `ro.hardware.<module name>` to change the platform
name of a module that needs to be searched for.  On all our platforms
the Vulkan library is called `vulkan.qcom.so` instead of
`vulkan.$(TARGET_BOARD_PLATFORM).so` and Android can simply be taught to
search for the former instead of the latter by setting
`ro.hardware.vulkan=qcom`.

[1]: https://github.com/sonyxperiadev/bug_tracker/issues/718

Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

hardware/adreno/Android.mk
@@ -18,19 +18,13 @@ library_names := \
     libllvm-glnext.so \
     libllvm-qcom.so \
     librs_adreno.so \
-    librs_adreno_sha1.so \
-    hw/vulkan.qcom.so
+    librs_adreno_sha1.so
 
 # Create symlinks to 32- and 64-bit directories:
 SONY_SYMLINKS := $(foreach lib_dir,lib lib64, \
     $(foreach p,$(library_names), \
         /odm/$(lib_dir)/$p:$(TARGET_COPY_OUT_VENDOR)/$(lib_dir)/$p \
     ) \
-)
-
-# Special exception for vulkan.qcom.so that is also linked as vulkan.$(TARGET_BOARD_PLATFORM).so
-SONY_SYMLINKS += $(foreach lib_dir,lib lib64, \
-    /odm/$(lib_dir)/hw/vulkan.qcom.so:$(TARGET_COPY_OUT_VENDOR)/$(lib_dir)/hw/vulkan.$(TARGET_BOARD_PLATFORM).so \
 )
 
 include $(SONY_BUILD_SYMLINKS)

common-prop.mk
@@ -303,3 +303,7 @@ endif
 # Reduce cost of scrypt for FBE CE decryption
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.scrypt_params=15:3:1
+
+# Look for vulkan.qcom.so instead of vulkan.$(BOARD_TARGET_PLATFORM).so
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.vulkan=qcom

commit d33ce7d0eb10e7d63674c55c95b0a88ff3c17206

hardware/adreno: Add back vendor->odm symlink for `vulkan.qcom.so`
Commit 0aecc439 ("hardware/adreno: Convert vendor->odm symlink to
ro.hardware property") cleaned up the Vulkan Adreno blob links by using
`ro.hardware.vulkan=qcom` to search for a qcom-vendored blob instead of
one specifically named after the current board.  However, this also
dropped the symlink from vendor to odm which is necessary for Android to
find the module.  Android loads this hardware module from the sphal
namespace [1] which is allowed to load from both /odm/${LIB} and
/vendor/${LIB}, but is only searching in the hw/ subdirectory of
/vendor/${LIB} [2].

Directly symlinking from /odm/${LIB} into /odm/${LIB}/hw is possible
however since sphal _is_ instructed to _search_ there, but this requires
and odm change and a new release; instead restore the previous symlink
from /vendor's hw/ subfolder into /odm's hw/ subfolder.

[1]: https://cs.android.com/android/platform/superproject/+/android-11.0.0_r40:frameworks/native/vulkan/libvulkan/driver.cpp;l=182
[2]: https://cs.android.com/android/platform/superproject/+/android-11.0.0_r40:system/linkerconfig/contents/namespace/sphal.cc;l=41-44

Fixes: 0aecc439 ("hardware/adreno: Convert vendor->odm symlink to ro.hardware property")
Signed-off-by: Marijn Suijten <marijns95@gmail.com>

hardware/adreno/Android.mk
@@ -18,7 +18,8 @@ library_names := \
     libllvm-glnext.so \
     libllvm-qcom.so \
     librs_adreno.so \
-    librs_adreno_sha1.so
+    librs_adreno_sha1.so \
+    hw/vulkan.qcom.so
 
 # Create symlinks to 32- and 64-bit directories:
 SONY_SYMLINKS := $(foreach lib_dir,lib lib64, \