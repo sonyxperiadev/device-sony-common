sonyxperiadev/device-sony-common

commit f570b264310e48fa69869ff37b027f74c12fde7a

common: Update vendor boot variable
See https://r.android.com/1457443

The variable also needs to be set earlier now since it's
read-only by the time CommonConfig.mk is read.

Groundhog day, yada yada. The CADT is strong with El Goog.

common.mk
@@ -52,6 +52,10 @@ PRODUCT_BUILD_RECOVERY_IMAGE := true
 # Force building a boot image. This needs to be set explicitly since Android R
 PRODUCT_BUILD_BOOT_IMAGE := true
 
+# Android R: Disable logic for new vendor_boot
+# Our devices do not support it
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
+
 KERNEL_PATH := kernel/sony/msm-$(SOMC_KERNEL_VERSION)
 # Sanitized prebuilt kernel headers
 -include $(KERNEL_PATH)/common-headers/KernelHeaders.mk

CommonConfig.mk
@@ -27,10 +27,6 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RECOVERY ?= false
 TARGET_NO_KERNEL := false
 
-# Android R: Disable logic for new vendor_boot
-# Our devices do not support it
-TARGET_NO_VENDOR_BOOT := true
-
 # common cmdline parameters
 ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive

commit 6f4af0ee1e939f3e9c4fe60691285bc0b230dbdf

common: Don't explicitly disable vendor boot
None of our devices set BOARD_BOOT_HEADER_VERSION, which must be at
least version 3 for vendor_boot to be enabled by default, as per the
AOSP change that this was originally explicitly disabled for:
https://r.android.com/1457443 - it'll stay disabled for all our
platforms without explicitly setting this to `false`.
Removing this explicit disablement will allow us to enable the feature
on newer platforms like Sagami where vendor_boot is required.

Build- and boot-tested on XQ-AU52 (PDX201 DSDS, Seine, Xperia 10 II)
running Android 12, and J9110 (Griffin DSDS, Kumano, Xperia 1) running
Android 11, no vendor_boot.img is generated and the device functions as
normal.

Fixes: f570b264 ("common: Update vendor boot variable")
Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

common.mk
@@ -68,10 +68,6 @@ PRODUCT_PACKAGES += \
 # Force building a recovery image: Needed for OTA packaging to work since Q
 PRODUCT_BUILD_RECOVERY_IMAGE := true
 
-# Android R: Disable logic for new vendor_boot
-# Our devices do not support it
-PRODUCT_BUILD_VENDOR_BOOT_IMAGE := false
-
 KERNEL_PATH := kernel/sony/msm-$(SOMC_KERNEL_VERSION)
 # Sanitized prebuilt kernel headers
 -include $(KERNEL_PATH)/common-headers/KernelHeaders.mk