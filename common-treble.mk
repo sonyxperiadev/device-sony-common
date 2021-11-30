456a075a364a3ede9e3caca9e11fa5ce29499e17
Alin Jerpelea <alin.jerpelea@sony.com>
2021-11-30 13:39:25 +0000

    Revert "common-treble: Provide latest radio@1.6 and radio.config@1.3 HAL libs"
    This reverts commit 59f27aab21359e25a27fb55fc6bad0ad78132b93.

--- common-treble.mk
+++ common-treble.mk
@@ -39,8 +39,8 @@ PRODUCT_PACKAGES += \
 # RIL
 # Interface library needed by odm blobs:
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.6 \
-    android.hardware.radio.config@1.3
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.2
 
 # Audio
 PRODUCT_PACKAGES += \
