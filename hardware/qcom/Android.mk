23420759d0c93f13d53b37769cdb23941943b3d7
Alin Jerpelea <alin.jerpelea@sony.com>
2021-12-07 14:38:57 +0000

    qcom: add support for sm8350 SOC
    Add support for sm8350 SOC and 5.4 kernel,
    used by Sony Xperia 1 III and Sony Xperia 5 III (sagami board)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -3,18 +3,18 @@ ifneq ($(wildcard device/sony/common/hardware/qcom/custom.mk),)
 else
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
-QCOM_BOARD_PLATFORMS += msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+QCOM_BOARD_PLATFORMS += msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 # Some supported platforms need a different media hal
 # This list selects platforms that should use the latest media hal
 # All other platforms automatically fallback to the legacy hal
-QCOM_NEW_MEDIA_PLATFORM := sdm845 sm6125 sm6350 sm8150 sm8250
+QCOM_NEW_MEDIA_PLATFORM := sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+MSM_VIDC_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 #List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+MASTER_SIDE_CP_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 ifneq ($(filter $(QCOM_NEW_MEDIA_PLATFORM), $(TARGET_BOARD_PLATFORM)),)
 QCOM_MEDIA_ROOT := vendor/qcom/opensource/media/sm8150
