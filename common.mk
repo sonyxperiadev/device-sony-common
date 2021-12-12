sonyxperiadev/device-sony-common

commit 367ae94e40629f2cff8c43be8fea3fded739e82f

qcom: add support for sm8305 SOC
Add support for sm8350 SOC and 5.4 kernel,
used by Sony Xperia 1 III and Sony Xperia 5 III (sagami board)

Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

common.mk
@@ -15,11 +15,15 @@
 # Common path
 COMMON_PATH := device/sony/common
 
+ifneq ($(filter 4.19, $(SOMC_KERNEL_VERSION)),)
+display_platform := sm8250
+else
+display_platform := sm8350
+endif
+
 # Everything prior to kernel 4.19 uses the sm8150 display HAL
 ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
 display_platform := sm8150
-else
-display_platform := sm8250
 endif
 
 # Enable building packages from device namspaces.

hardware/qcom/Android.mk
@@ -3,18 +3,18 @@ ifneq ($(wildcard device/sony/common/hardware/qcom/custom.mk),)
 else
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
-QCOM_BOARD_PLATFORMS += msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+QCOM_BOARD_PLATFORMS += msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 # Some supported platforms need a different media hal
 # This list selects platforms that should use the latest media hal
 # All other platforms automatically fallback to the legacy hal
-QCOM_NEW_MEDIA_PLATFORM := sdm845 sm6125 sm6350 sm8150 sm8250
+QCOM_NEW_MEDIA_PLATFORM := sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+MSM_VIDC_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 #List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250
+MASTER_SIDE_CP_TARGET_LIST := msm8998 sdm660 sdm845 sm6125 sm6350 sm8150 sm8250 sm8350
 
 ifneq ($(filter $(QCOM_NEW_MEDIA_PLATFORM), $(TARGET_BOARD_PLATFORM)),)
 QCOM_MEDIA_ROOT := vendor/qcom/opensource/media/sm8150

commit 0c5868ed8fd3d27609568e7526e4ca406fa90be4

common: Clean up unnecessarily complex ifneq switches for display
Fixes: 367ae94e ("qcom: add support for sm8305 SOC")

common.mk
@@ -15,15 +15,12 @@
 # Common path
 COMMON_PATH := device/sony/common
 
-ifneq ($(filter 4.19, $(SOMC_KERNEL_VERSION)),)
+ifeq ($(SOMC_KERNEL_VERSION), 4.14)
+display_platform := sm8150
+else ifeq ($(SOMC_KERNEL_VERSION), 4.19)
 display_platform := sm8250
 else
 display_platform := sm8350
-endif
-
-# Everything prior to kernel 4.19 uses the sm8150 display HAL
-ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
-display_platform := sm8150
 endif
 
 # Enable building packages from device namspaces.