sonyxperiadev/device-sony-common

commit 1a91f1b6adb3762aea19e2838a1c01141cf5d5ed

Upgrade IBootControl HAL to 1.1
FCM5 deprecates 1.0 and mandates 1.1.  1.1 also includes a vintf
fragment.

Signed-off-by: Marijn Suijten <marijns95@gmail.com>

common-treble.mk
@@ -136,10 +136,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.sony
 
+# Only define bootctrl HAL availability on AB platforms:
 ifeq ($(AB_OTA_UPDATER),true)
-# Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
 endif

CommonConfig.mk
@@ -212,11 +212,6 @@ else
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hw.keymaster_v3.xml
 endif
 
-# Only define bootctrl HAL availability on AB platforms:
-ifeq ($(AB_OTA_UPDATER),true)
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hardware.bootctrl.xml
-endif
-
 ifeq ($(TARGET_VIBRATOR_V1_2),true)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hardware.vibrator_v1.2.xml
 else

vintf/android.hardware.bootctrl.xml
@@ -1,12 +1,0 @@
-<manifest version="1.0" type="device">
-    <hal format="hidl">
-        <name>android.hardware.boot</name>
-        <transport>hwbinder</transport>
-        <version>1.0</version>
-        <interface>
-            <name>IBootControl</name>
-            <instance>default</instance>
-        </interface>
-    </hal>
-</manifest>
-

commit 12ac29e908d3b6fb541299af41fa7a0ce55b7afa

common-treble: Switch to CAF boot HAL 1.1
AOSP switched up the default implementation of the bootctrl 1.1 HAL from
GPT flags to the `/misc` partition (`-impl` lib doesn't load the
bootctrl libhardware module anymore but delegates to
`libboot_control.so`) [1], which is incorrect on our Qualcomm boards.
As we'd still like to switch to the 1.1 interface for FCM5
compatibility, and would like to drop the legacy libhardware module (see
`bootctl.<platform>` in the respective platform directories, and its
implementation in `hardware/qcom/bootctrl`) we'll use the updated CAF
repository directly.

Note that we still include AOSP's 1.1 passthrough library loader for
lack of a startable service in the CAF repo.  As usual, this provides us
with the right init .rc and vintf fragment, and allows starting both a
1.0 and 1.1 passthrough (`-impl`) library.  It is this `-impl` library
that is custom to QTI and calls into their custom
`libboot_control_qti.so`.

[1]: https://github.com/sonyxperiadev/bug_tracker/issues/740#issuecomment-1016325638

Fixes: 1a91f1b6 ("Upgrade IBootControl HAL to 1.1")
Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

common-treble.mk
@@ -139,7 +139,7 @@ PRODUCT_PACKAGES += \
 # Only define bootctrl HAL availability on AB platforms:
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl \
-    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.1-service
 endif