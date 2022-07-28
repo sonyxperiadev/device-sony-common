51f62c6ba0faadfe859a6a62066d1deed6817aa4
Alin Jerpelea <alin.jerpelea@sony.com>
2022-07-28 11:04:50 +0200

    CommonConfig: remove deprecated GRALLOC1
    Everything prior to kernel 4.19 uses the sm8150 display HAL and GRALLOC1
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common.mk
+++ common.mk
@@ -21,11 +21,6 @@ else
 display_platform := sm8350
 endif
 
-# Everything prior to kernel 4.19 uses the sm8150 display HAL
-ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
-display_platform := sm8150
-endif
-
 # Enable building packages from device namspaces.
 # Might be temporary! See:
 # https://android.googlesource.com/platform/build/soong/+/master/README.md#name-resolution
