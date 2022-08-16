86e3f82f225fe0b860cc0f21930b4a31577fa1ab
Alin Jerpelea <alin.jerpelea@sony.com>
2022-08-16 15:02:35 +0200

    common-sanitizer.mk: remove platform specific options
    all our platforms are using the same sanitizer options
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-sanitizer.mk
+++ common-sanitizer.mk
@@ -2,17 +2,3 @@ PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/interfaces/camera\
     vendor/qcom/proprietary/camx \
     vendor/qcom/proprietary/chi-cdk
-
-ifneq ($(filter sagami,$(SOMC_PLATFORM)),)
-PRODUCT_INTEGER_OVERFLOW_INCLUDE_PATHS += \
-    hardware/interfaces\
-    hardware/qcom \
-    vendor/qcom/proprietary \
-    vendor/qcom/opensource
-
-PRODUCT_BOUNDS_INCLUDE_PATHS += \
-    hardware/interfaces\
-    hardware/qcom \
-    vendor/qcom/proprietary \
-    vendor/qcom/opensource
-endif
