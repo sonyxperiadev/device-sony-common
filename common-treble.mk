916d640adc7f2b41a8bbe2bf1b059f907e4f655e
Alin Jerpelea <alin.jerpelea@sony.com>
2023-09-13 11:00:01 +0200

    vintf: adjust telephony interfaces
    adjust the versions to match the latest software delivery
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-treble.mk
+++ common-treble.mk
@@ -39,8 +39,8 @@ PRODUCT_PACKAGES += \
 # RIL
 # Interface library needed by odm blobs:
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
