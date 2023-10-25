80a14ce2390bf44d6b996b8ae980231da4852f81
ShujathMohd <shujath@outlook.in>
2023-10-25 15:10:54 +0530

    common : drop thermal 1.0 which is deprecated in FCM 4 Android 9 and lower use a polling interface defined in Thermal HAL 1.0 to get temperature readings. This HAL allowed the Android framework and other trusted clients, such as a device manufacturer's HAL, to read the current temperature and product-policy specific throttling and shutdown thresholds for each sensor through the same API.
    Android 10 introduced a thermal system in the Android framework and a new version of the HAL, Thermal HAL 2.0, that abstracts the interface to the thermal subsystem hardware devices. The hardware interface includes temperature sensors and thermistors for the skin, battery, GPU, CPU, and USB port..

--- common-treble.mk
+++ common-treble.mk
@@ -136,9 +136,7 @@ endif
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0 \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0
 
 # Power
 PRODUCT_PACKAGES += \
