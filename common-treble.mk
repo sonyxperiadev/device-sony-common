b66cdadfca89a77755e8704d6460055295cec362
Gabriel Biren <gbiren@google.com>
2023-10-25 12:45:13 +0530

    common_treble : Use AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- common-treble.mk
+++ common-treble.mk
@@ -76,7 +76,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd@1.0.vendor
 
 # NFC packages
