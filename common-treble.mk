72741b29cb5a78c57b65d5b63bb575fb133cf523
Alin Jerpelea <alin.jerpelea@sony.com>
2024-06-28 14:58:19 +0200

    treble: bump radio.config version
    5.15 devices need 1.3 while 5.4 devices can use 1.2
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-treble.mk
+++ common-treble.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
 # Interface library needed by odm blobs:
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
