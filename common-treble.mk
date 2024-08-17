4f3bbcd9cd97d321550e4631bb8b46af637f3ff5
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-18 02:16:33 +0300

    common-treble: Bump audio effect implementation to 7.0
    Bump audio effect implementation to 7.0 version.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-treble.mk
+++ common-treble.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
