c4a1858b96ff4f13468f1c1b2e5544823c904886
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-18 02:21:20 +0300

    common-treble: Switch BT audio to AIDL
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-treble.mk
+++ common-treble.mk
@@ -47,7 +47,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl:32 \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl
 
 # Camera
