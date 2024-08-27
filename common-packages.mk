3de6935f541cca2faa242d9b4f5392383c211ec6
Pavel Dubrova <pashadubrova@gmail.com>
2024-08-27 21:23:18 +0300

    packages: Add PAL codec plugins
    Codec lib and codec format which they represent:
    lib_bt_aptx:   aptX
    lib_bt_ble:    LC3
    lib_bt_bundle: AAC, SBC, LDAC
    
    Packages are protected with the TARGET_USES_AUDIOREACH flag.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-packages.mk
+++ common-packages.mk
@@ -51,6 +51,12 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin
+
+# PAL
+PRODUCT_PACKAGES += \
+    lib_bt_aptx \
+    lib_bt_ble \
+    lib_bt_bundle
 endif
 
 # GFX
