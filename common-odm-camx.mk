a3a4dfba9746536f83fbac50eebfb43ee8326427
Alin Jerpelea <alin.jerpelea@sony.com>
2025-04-15 16:19:12 +0200

    common-odm-camx: add missing libraries for 5.10 camera
    5.10 camera comes with new dependencies for columbia platform
    used by Xperia 10 VI
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- common-odm-camx.mk
+++ common-odm-camx.mk
@@ -1,114 +1,173 @@
 # CAMX Libs
 ifeq ($(filter seine,$(SOMC_PLATFORM)),)
 PRODUCT_PACKAGES += \
-    libqshcamera\
-    libcamxhwlcvp \
-    libcamxhwlqsat \
-    libcamxhwltitan17x \
-    libcamxhwlfd \
-    libcamera_nn_stub \
-    libcamerapostproc \
-    libcamerapostproc.bitra \
-    libcamxexternalformatutils \
-    libcom.qti.chinodeutils \
+    camera.qcom \
+    camx.device@3.2-impl \
+    camx.device@3.3-impl \
+    camx.device@3.4-ext-impl \
+    camx.device@3.4-impl \
+    camx.device@3.5-ext-impl \
+    camx.device@3.5-impl \
+    camx.device@3.6-ext-impl \
+    camx.device@3.6-impl \
+    camx.device@3.7-impl \
+    camx.provider@2.4-external \
+    camx.provider@2.4-impl \
+    camx.provider@2.4-legacy \
+    camx.provider@2.5-external \
+    camx.provider@2.5-legacy \
+    camx.provider@2.6-legacy \
+    camx.provider@2.7-legacy \
     com.qti.camx.chiiqutils \
     com.qti.chi.override \
     com.qti.chi.override.bitra \
     com.qti.chiusecaseselector \
     com.qti.feature2.afbrckt \
     com.qti.feature2.anchorsync \
+    com.qti.feature2.anchorsync.bitra \
     com.qti.feature2.demux \
+    com.qti.feature2.demux.bitra \
+    com.qti.feature2.derivedoffline \
     com.qti.feature2.frameselect \
+    com.qti.feature2.frameselect.bitra \
     com.qti.feature2.fusion \
+    com.qti.feature2.fusion.bitra \
     com.qti.feature2.generic \
+    com.qti.feature2.generic.bitra \
     com.qti.feature2.gs \
+    com.qti.feature2.gs.bitra \
+    com.qti.feature2.gs.netrani \
     com.qti.feature2.gs.sm6375 \
+    com.qti.feature2.gs.sm6450 \
     com.qti.feature2.gs.sm8350 \
     com.qti.feature2.gs.sm8450 \
     com.qti.feature2.gs.sm8550 \
     com.qti.feature2.hdr \
+    com.qti.feature2.hdr.bitra \
     com.qti.feature2.mcreprocrt \
     com.qti.feature2.memcpy \
+    com.qti.feature2.memcpy.bitra \
     com.qti.feature2.mfsr \
+    com.qti.feature2.mfsr.bitra \
+    com.qti.feature2.mfsr.netrani \
+    com.qti.feature2.mfsr.sm8450 \
+    com.qti.feature2.ml \
+    com.qti.feature2.ml.fillmore \
     com.qti.feature2.mux \
     com.qti.feature2.qcfa \
+    com.qti.feature2.qcfa.bitra \
     com.qti.feature2.rawhdr \
+    com.qti.feature2.rawhdr.bitra \
     com.qti.feature2.realtimeserializer \
     com.qti.feature2.rt \
+    com.qti.feature2.rt.bitra \
     com.qti.feature2.rtmcx \
     com.qti.feature2.serializer \
-    com.qti.feature2.stub \
-    com.qti.feature2.swmf \
-    com.qti.feature2.anchorsync.bitra \
-    com.qti.feature2.demux.bitra \
-    com.qti.feature2.frameselect.bitra \
-    com.qti.feature2.fusion.bitra \
-    com.qti.feature2.rt.bitra \
-    com.qti.feature2.memcpy.bitra \
-    com.qti.feature2.mfsr.bitra \
-    com.qti.feature2.qcfa.bitra \
-    com.qti.feature2.hdr.bitra \
-    com.qti.feature2.generic.bitra \
-    com.qti.feature2.rawhdr.bitra \
     com.qti.feature2.serializer.bitra \
+    com.qti.feature2.statsregeneration \
+    com.qti.feature2.stub \
     com.qti.feature2.stub.bitra \
+    com.qti.feature2.swmf \
     com.qti.feature2.swmf.bitra \
-    com.qti.feature2.gs.bitra \
-    com.qti.hvx.binning \
     com.qti.hvx.addconstant \
-    com.qti.node.dummydepth \
-    com.qti.node.eisv2 \
-    com.qti.node.eisv3 \
-    com.qti.node.gme \
-    com.qti.node.swregistration \
-    com.qti.node.memcpy \
-    com.qti.node.dummyrtb \
-    com.qti.node.dummysat \
+    com.qti.hvx.binning \
+    com.qti.node.afbfusion \
+    com.qti.node.customhwnode \
     com.qti.node.depth \
     com.qti.node.dewarp \
-    com.qti.node.formatconversion \
+    com.qti.node.dummydepth \
+    com.qti.node.dummyrtb \
+    com.qti.node.dummysat \
+    com.qti.node.eisv2 \
+    com.qti.node.eisv3 \
     com.qti.node.fcv \
+    com.qti.node.formatconversion \
+    com.qti.node.gme \
     com.qti.node.gpu \
+    com.qti.node.gyrornn \
+    com.qti.node.hdr10pgen \
+    com.qti.node.hdr10phist \
+    com.qti.node.memcpy \
     com.qti.node.ml \
+    com.qti.node.mlinference \
+    com.qti.node.muxer \
     com.qti.node.remosaic \
     com.qti.node.stich \
+    com.qti.node.sw2dlut \
     com.qti.node.swaidenoiser \
     com.qti.node.swbestats \
     com.qti.node.swcac \
+    com.qti.node.swec \
+    com.qti.node.swfusion \
+    com.qti.node.swhme \
     com.qti.node.swlsc \
     com.qti.node.swmctf \
     com.qti.node.swpdpc \
     com.qti.node.swpreprocess \
+    com.qti.node.swregistration \
+    com.qti.node.swvrt \
+    com.qti.settings.clarence \
+    com.qti.settings.fillmore \
+    com.qti.settings.netrani \
     com.qti.settings.sm6350 \
     com.qti.settings.sm6375 \
+    com.qti.settings.sm6450 \
     com.qti.settings.sm8150 \
     com.qti.settings.sm8250 \
     com.qti.settings.sm8350 \
     com.qti.settings.sm8450 \
     com.qti.settings.sm8550 \
-    com.qti.stats.common \
     com.qti.stats.aec \
     com.qti.stats.aecwrapper \
     com.qti.stats.af \
     com.qti.stats.afd \
     com.qti.stats.afwrapper \
     com.qti.stats.awbwrapper \
+    com.qti.stats.common \
     com.qti.stats.hafoverride \
     com.qti.tuned.default \
     com.qtistatic.stats.aec \
     com.qtistatic.stats.af \
     com.qtistatic.stats.awb \
     com.qtistatic.stats.pdlib \
+    com.qualcomm.mcx.distortionmapper \
+    com.qualcomm.mcx.linearmapper \
     com.qualcomm.mcx.nonlinearmapper \
-    libdepthmapwrapper \
+    com.qualcomm.mcx.policy.mfl \
+    com.qualcomm.mcx.policy.xr \
+    com.qualcomm.qti.mcx.usecase.extension \
     fdconfigpreview \
     fdconfigpreviewlite \
     fdconfigvideo \
     fdconfigvideolite \
+    libcamera_nn_stub \
+    libcamerapostproc \
+    libcamerapostproc.bitra \
+    libcamxcommonutils \
+    libcamxcore \
+    libcamxexternalformatutils \
+    libcamxhwlcvp \
+    libcamxhwlfd \
+    libcamxhwlqsat \
+    libcamxhwltitan17x \
+    libcamximageformatutils \
+    libcamxsettingsmanager \
+    libcamxswispiqmodule \
+    libchilog \
+    libcom.qti.chinodeutils \
+    libdepthcomputation \
+    libdepthmapwrapper \
+    libdepthmapwrapper_secure \
+    libeepromcutter \
+    libopencv \
+    libqshcamera\
+    libspectre \
+    vendor.qti.camera.provider \
+    vendor.qti.camera.provider-service_64 \
     vendor.qti.hardware.camera.aon@1.0-service-impl \
     vendor.qti.hardware.camera.aon@1.3 \
     vendor.qti.hardware.camera.postproc@1.0 \
-    camera.qcom
+    vendor.qti.hardware.camera.postproc@1.0-service-impl
 endif
 
 ifeq ($(SOMC_PLATFORM),edo)
@@ -189,6 +248,17 @@ PRODUCT_PACKAGES += \
     com.sony.eeprom.zambezi_tele \
     com.sony.eeprom.zambezi_uwide \
     com.sony.eeprom.zambezi_wide \
+    com.sony.sensor.hi846 \
+    com.sony.sensor.hi847 \
+    com.sony.sensor.imx582
+endif
+
+ifeq ($(SOMC_PLATFORM),columbia)
+PRODUCT_PACKAGES += \
+    com.sony.eeprom.columbia_front \
+    com.sony.eeprom.columbia_tele \
+    com.sony.eeprom.columbia_uwide \
+    com.sony.eeprom.columbia_wide \
     com.sony.sensor.hi846 \
     com.sony.sensor.hi847 \
     com.sony.sensor.imx582
