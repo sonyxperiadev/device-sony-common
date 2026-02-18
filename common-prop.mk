801bea32ffca80178d589506b69c2eb24b81c78c
Pavel Dubrova <pashadubrova@gmail.com>
2026-02-18 04:52:47 +0200

    common-prop: Set ro.lmk.use_new_strategy for PSI based LMK
    Set ro.lmk.use_new_strategy to use Pressure Stall
    Information (PSI) metrics to detect high memory
    pressure and trigger process killing.
    
    Moreover, legact kill strategy cannot be enabled with
    Control Group v2.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- common-prop.mk
+++ common-prop.mk
@@ -143,7 +143,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.downgrade_pressure=100 \
     ro.lmk.kill_heaviest_task=true \
     ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true
+    ro.lmk.use_minfree_levels=true \
+    ro.lmk.use_new_strategy=true
 
 # Property to enable user to access Google WFD settings.
 PRODUCT_PROPERTY_OVERRIDES += \
